48baf63c6ff36ca0f3b424c78955b637d39fb4f1
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-05 13:52:53 -0700

    Add First Steps In Coding Lab

---
+++ Programing basics C#/01. First Steps In Coding/03. Nums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
