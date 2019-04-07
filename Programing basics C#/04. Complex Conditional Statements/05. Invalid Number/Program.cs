293ed8652d5930d4586fd9748c6f85fbfe90728a
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-07 02:31:09 -0700

    Add 05. Invalid Number Problem

---
+++ Programing basics C#/04. Complex Conditional Statements/05. Invalid Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            bool isValid;
+            if (num >= 100 && num <= 200)
+            {
+                isValid = true;
+            }
+            else if (num == 0) {
+                isValid = true;
+            }
+            else{
+                isValid = false;
+            }
+
+            if (!isValid)//if (!isValid) =======  if(isValid == false)
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
