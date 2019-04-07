c8b4a414229cc8e7fcf629a519c45d51164a85f2
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-07 02:29:38 -0700

    Add 04. Fruit or Vegetable Problem

---
+++ Programing basics C#/04. Complex Conditional Statements/04. Fruit or Vegetable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "banana" || input == "apple" || input == "kiwi" || input == "cherry" || input == "lemon" || input == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (input == "carrot" || input == "pepper" || input == "cucumber" || input == "tomato")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
