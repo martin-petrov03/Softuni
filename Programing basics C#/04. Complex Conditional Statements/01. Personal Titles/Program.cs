a3e7614c3a09c106b5964a336ea2ce53e98a4a09
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-07 02:23:11 -0700

    Add 01. Personal Titles Problem

---
+++ Programing basics C#/04. Complex Conditional Statements/01. Personal Titles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01.Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            char sex = char.Parse(Console.ReadLine());
+
+            string salutation = "";
+
+            if (sex == 'm')
+            {
+                if (age < 16)
+                {
+                    salutation = "Master";
+                }
+                else {
+                    salutation = "Mr.";
+                }
+            }
+            else if (sex == 'f')
+            {
+                if (age < 16)
+                {
+                    salutation = "Miss";
+                }
+                else
+                {
+                    salutation = "Ms.";
+                }
+            }
+            else {
+                Console.WriteLine("Death");
+            }
+            Console.WriteLine(salutation);
+        }
+    }
+}
