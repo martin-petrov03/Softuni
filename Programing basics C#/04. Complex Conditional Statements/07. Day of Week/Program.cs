938c23d0524264771439c6f57e9f3f791994e969
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-07 02:34:18 -0700

    Add 07. Day of Week Problem

---
+++ Programing basics C#/04. Complex Conditional Statements/07. Day of Week/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09.Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string dayWithWords = "";
+            switch (day) {
+                case 1:
+                    dayWithWords = "Monday";
+                    break;
+                case 2:
+                    dayWithWords = "Tuesday";
+                    break;
+                case 3:
+                    dayWithWords = "Wednesday";
+                    break;
+                case 4:
+                    dayWithWords = "Thursday";
+                    break;
+                case 5:
+                    dayWithWords = "Friday";
+                    break;
+                case 6:
+                    dayWithWords = "Saturday";
+                    break;
+                case 7:
+                    dayWithWords = "Sunday";
+                    break;                
+                default:
+                    dayWithWords = "Error";
+                    break;
+            }
+            Console.WriteLine(dayWithWords);
+        }
+    }
+}
