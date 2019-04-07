28491885ab1a1fe46a7d9a0d6c0663c419d9641f
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-07 02:38:57 -0700

    Add 10. Volleyball Problem

---
+++ Programing basics C#/04. Complex Conditional Statements/10. Volleyball/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _12.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeYear = Console.ReadLine();
+            int countWeekendsForVolleyball = int.Parse(Console.ReadLine());
+            int countWeekendsForTravel = int.Parse(Console.ReadLine());
+
+            int sumOfWeekends = countWeekendsForVolleyball + countWeekendsForTravel;
+            if (typeYear == "leap")
+            {
+                double wekkendsWithoutTravel = (48 - countWeekendsForTravel);
+                double weekendsInSofiaWithVolleyball = wekkendsWithoutTravel * 3 / 4d;
+                //Console.WriteLine($"{weekendsInSofiaWithVolleyball:F7}");
+                double weekendsInHomeTown = countWeekendsForTravel;
+                //Console.WriteLine($"{weekendsInHomeTown:F7}");
+                double playsInHolidays = countWeekendsForVolleyball * 2 / 3d;
+                //Console.WriteLine($"{playsInHolidays:F7}");
+                double bonusPlays = 15 / 100d * (playsInHolidays + weekendsInHomeTown + weekendsInSofiaWithVolleyball);
+                //Console.WriteLine($"{bonusPlays:F7}");
+                double allPlayTimes = weekendsInSofiaWithVolleyball + bonusPlays + weekendsInHomeTown + playsInHolidays;
+
+                Console.WriteLine(Math.Floor(allPlayTimes));
+            }
+            else if (typeYear == "normal") {
+               double weekendsInSofiaWithVolleyball = (48 - countWeekendsForTravel) * 3 / 4d;
+                double weekendsInHomeTown = countWeekendsForTravel;
+                double playsInHolidays = countWeekendsForVolleyball * 2 / 3d;
+                double allPlayTimes = weekendsInSofiaWithVolleyball + weekendsInHomeTown + playsInHolidays;
+
+                Console.WriteLine(Math.Floor(allPlayTimes));
+            }
+        }
+    }
+}
