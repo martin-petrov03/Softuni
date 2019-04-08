e272bf884faf1a524d5e1bfa7217b9a0c6541ab2
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-08 08:48:23 -0700

    Add 08. Sequence 2k+1 Problem

---
+++ Programing basics C#/05. While-Loop/08. Sequence 2k+1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08.Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            int i = 1;
+            Console.WriteLine(i);
+            while (true) {
+                int result = 2 * i + 1;
+                if (result <= inputNum)
+                {
+                    Console.WriteLine(result);
+                }
+                else {
+                    break;
+                }
+                i = result;
+            }
+        }
+    }
+}
