51b3da91518d209b41604cf788696cae5372191c
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-08 08:50:29 -0700

    Add 09. Greatest Common Divisor (CGD) Problem

---
+++ Programing basics C#/05. While-Loop/09. Greatest Common Divisor (CGD)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _09.Greatest_Common_Divisor__CGD_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Gcd(a, b));
+        }
+        static int Gcd(int a, int b) {
+            int gcd = 0;
+            while (a != b) {
+                if (a > b) {
+                    a -= b;
+                }
+                else {
+                    b -= a;
+                }
+            }            
+                gcd = a;
+                return gcd;                    
+        }
+    }
+}
