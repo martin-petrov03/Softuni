051b374e4c9b74ff751ceac3f381be7cd7078ceb
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-08 08:45:05 -0700

    Add 06. Account Balance Problem

---
+++ Programing basics C#/05. While-Loop/06. Account Balance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int installmentCount = int.Parse(Console.ReadLine());
+            double totalBalance = 0;
+            int i = 1;
+            while (i <= installmentCount) {
+                double currentInstallment = double.Parse(Console.ReadLine());
+                if (currentInstallment < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                else
+                {
+                    totalBalance += currentInstallment;
+                    Console.WriteLine($"Your account balance was increased by: {currentInstallment:f2}");
+                }
+                i++;
+            }
+            Console.WriteLine($"Total balance: {totalBalance:f2}");
+        }
+    }
+}
+
