e1409f5d5263cb63b260ba8f705c1a6098ce0571
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-08 08:46:27 -0700

    Add 07. Water dispenser Problem

---
+++ Programing basics C#/05. While-Loop/07. Water dispenser/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07.Water_dispenser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int glassVolume = int.Parse(Console.ReadLine());
+            int tapp = 0;
+            while (true) {
+                string command = Console.ReadLine();
+                if (command == "Easy")
+                {
+                    tapp++;
+                    glassVolume -= 50;
+                } else if (command == "Medium") {
+                    tapp++;
+                    glassVolume -= 100;
+                } else if (command == "Hard") {
+                    tapp++;
+                    glassVolume -= 200;
+                }
+                if (glassVolume <= 0) { break; }
+            }
+            if (glassVolume == 0)
+            {
+                Console.WriteLine($"The dispenser has been tapped {tapp} times.");
+            }
+            else {
+                Console.WriteLine($"{0 - glassVolume}ml has been spilled.");
+            }
+        }
+    }
+}
