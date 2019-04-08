ef2c4c2942ac44d1e21f065716aa400e58c4f73b
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-08 08:37:41 -0700

    Add 01. Num Range 1 to 100 Problem

---
+++ Programing basics C#/05. While-Loop/01. Num Range 1 to 100/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Num_Range_1_to_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            while (num > 100 || num < 1)
+            {
+                Console.WriteLine("Invalid number!");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {num}");
+        }
+    }
+}
