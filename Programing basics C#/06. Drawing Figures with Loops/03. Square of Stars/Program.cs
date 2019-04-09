6ec5cce809cef4819ad60df094f1560d36b4b608
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:42:15 -0700

    Add 03. Square of Stars Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/03. Square of Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03.Square_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int cols = 1; cols <= n; cols++)
+            {
+                for (int rows = 1; rows <= n; rows++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
