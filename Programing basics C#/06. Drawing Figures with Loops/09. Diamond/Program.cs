dc3476e62e27b6fe8ac887ceab3c2210a88d9d1f
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:51:21 -0700

    Add 09. Diamond Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/09. Diamond/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _10.Diamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countDash = n;
+            
+            int height = n / 2;            
+            
+
+            int starsCount = 0;
+            if (n % 2 == 1)
+            {
+                starsCount = 1;
+            }
+            else
+            {
+                starsCount = 2;
+            }                               
+            
+            Console.WriteLine(new string('-', (n - starsCount) / 2) + new string('*', starsCount) + new string('-', (n - starsCount) / 2));
+            int j = starsCount;
+            int k = j;
+           
+            for (int row = 1; row <= height - 1; row++)
+            {
+                int m = (n - 2 - j) / 2;
+                Console.WriteLine(new string('-', m) + '*' + new string('-', j) + '*' + new string('-', m));                
+                j += 2;
+            }
+            //k -= starsCount;
+            for (int i = height - 1; i >= 1; i--)                     
+            {
+              
+                Console.WriteLine(new string('-', (n - 2 - k) / 2) + '*' + new string('-', k) + '*' + new string('-', (n - 2 - k) / 2));
+
+                k -= 2;
+            }
+
+           // Console.WriteLine(new string('-', (n - starsCount) / 2) + new string('*', starsCount) + new string('-', (n - starsCount) / 2));
+        }
+    }
+}
