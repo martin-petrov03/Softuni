1d1602baa29bdd463b1e2070d6cb6a8ffb4f90ef
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:36:03 -0700

    Add 01. Rectangle of Stars Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/01. Rectangle of Stars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Rectangle_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int cols = 1; cols <= 10; cols++)
+            {
+                for (int rows = 1; rows <= 10; rows++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
