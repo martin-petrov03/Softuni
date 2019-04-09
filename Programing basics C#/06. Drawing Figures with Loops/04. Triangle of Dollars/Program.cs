4779073ce912e8cccf452384a9397f02c64b8eba
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:43:31 -0700

    Add 04. Triangle of Dollars Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/04. Triangle of Dollars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.Triangle_of_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());           
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("$ ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
