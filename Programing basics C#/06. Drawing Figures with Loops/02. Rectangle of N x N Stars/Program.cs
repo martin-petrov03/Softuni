b409c452ee7233368119bc2fc689707875e18070
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:41:18 -0700

    Add 02. Rectangle of N x N Stars Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/02. Rectangle of N x N Stars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.Rectangle_of_N_x_N_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int cols = 1; cols <= n; cols++)
+            {
+                for (int rows = 1; rows <= n; rows++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
