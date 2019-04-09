6240d3a844c311a9b4099838477ba241829a7df5
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:44:58 -0700

    Add 05. Square Frame Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/05. Square Frame/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.Square_Frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //Head
+            Console.Write("+ ");
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.Write("+");
+            Console.WriteLine();
+
+            //Body
+            
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write("| ");                
+                for (int j = 0; j < n - 2; j++)
+                {
+                    Console.Write("- ");                    
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }            
+
+            //Footer
+            Console.Write("+ ");
+            for (int i = 1; i <= n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.Write("+");
+        }
+    }
+}
