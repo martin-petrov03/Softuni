56db41b34dab2f54a86b1ed3b80d1b8157e3b27b
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:53:21 -0700

    Add 10. Christmas Tree Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/10. Christmas Tree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.Christmas_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string(' ', n + 1) + '|');
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(
+                    new string(' ', n - i) + 
+                    new string('*', i) +
+                    " " + "|" + " " +
+                    new string('*', i) +
+                    new string(' ', n - 1));
+            }
+        }
+    }
+}
