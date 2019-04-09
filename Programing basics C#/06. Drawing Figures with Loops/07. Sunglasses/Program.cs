31eb0367a2983a46f0d66a2801bbbf1e6ebb65b9
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:47:17 -0700

    Add 07. Sunglasses Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/07. Sunglasses/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.Sunglasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //Header
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+
+            //Body
+            double averageRow = (n - 1) / 2;
+            for (int i = 1; i <= n - 2; i++)
+            {
+                if (i == Math.Ceiling(averageRow))
+                {
+                    Console.WriteLine('*' + new string('/', n * 2 - 2) + '*' + new string('|', n) + '*' + new string('/', n * 2 - 2) + '*');
+                }
+                else {
+                    Console.WriteLine('*' + new string('/', n * 2 - 2) + '*' + new string(' ', n) + '*' + new string('/', n * 2 - 2) + '*');
+                }                
+            }
+
+            //Footer
+            Console.WriteLine(new string('*', n * 2) + new string(' ', n) + new string('*', n * 2));
+        }
+    }
+}
