9b371d30c87e262d87df02b2cf27157e89b57e47
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-09 10:45:50 -0700

    Add 06. Rhombus of Stars Problem

---
+++ Programing basics C#/06. Drawing Figures with Loops/06. Rhombus of Stars/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+            // Upper Part 
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(new string(' ', n - i));
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }                 
+                Console.Write(new string(' ', n - i));
+                Console.WriteLine();
+            }
+            // Bottom Part
+          
+            for (int i = n - 1; i >= 1; i--)
+            {                
+                Console.Write(new string(' ', n - i));
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write(new string(' ', n - i));
+                Console.WriteLine();
+            }
+        }
+    }
+}
