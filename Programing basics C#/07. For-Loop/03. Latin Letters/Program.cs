9d467e5173d1e469bb513935ec61c4ce0ec526a1
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:03:04 -0700

    Add 03. Latin Letters Problem

---
+++ Programing basics C#/07. For-Loop/03. Latin Letters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03.Latin_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
