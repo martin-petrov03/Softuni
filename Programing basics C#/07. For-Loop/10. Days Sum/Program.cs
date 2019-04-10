a227ce568cd8b91a7467d26aec12697d7f5943ff
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:11:03 -0700

    Add 10. Days Sum Problem

---
+++ Programing basics C#/07. For-Loop/10. Days Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace _10.Days_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            DateTime firstData = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+            DateTime secondData = DateTime.ParseExact(Console.ReadLine(), "dd-MM-yyyy", null);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    firstData = firstData.AddDays(1);
+                }
+                else {
+                    secondData = secondData.AddDays(1);
+                }
+            }
+
+            int sumOfDays = firstData.Day + secondData.Day;
+
+            Console.WriteLine(sumOfDays);
+        }
+    }
+}
