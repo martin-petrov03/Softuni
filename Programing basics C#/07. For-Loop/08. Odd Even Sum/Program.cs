eee9aa8917579dcc6185ed157035b8ea9e64cfd4
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:08:43 -0700

    Add 08. Odd Even Sum Problem

---
+++ Programing basics C#/07. For-Loop/08. Odd Even Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int sumEven = 0;
+            int sumOdd = 0;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumEven += currentNum;
+                }
+                else {
+                    sumOdd += currentNum;
+                }
+            }            
+            if (sumEven == sumOdd)
+                Console.WriteLine($"Yes, sum = {sumEven}");
+            else
+                Console.WriteLine($"No, diff = {Math.Max(sumEven, sumOdd) - Math.Min(sumEven, sumOdd)}");
+        }
+    }
+}
