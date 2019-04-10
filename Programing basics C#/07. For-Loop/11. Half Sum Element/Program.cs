7f79dfd953256a86f3390fdd33a1b4f8eccab793
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:12:11 -0700

    Add 11. Half Sum Element Problem

---
+++ Programing basics C#/07. For-Loop/11. Half Sum Element/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _11.Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int maxNum = int.MinValue;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum > maxNum) {
+                    maxNum = currentNum;
+                }
+                sum += currentNum;
+            }
+            sum -= maxNum;
+            if (maxNum == sum)
+            {                                   
+                    Console.WriteLine($"Yes");
+                    Console.WriteLine($"sum = {sum}");                
+            }
+            else {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNum - sum)}");
+            }
+        }
+    }
+}
