9e0f30337968ea5f5edde7af2d6c471390d05416
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:04:17 -0700

    Add 04. Sum Numbers Problem

---
+++ Programing basics C#/07. For-Loop/04. Sum Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
