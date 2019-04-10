4e035afd430ff32189e75b2229cec7ba83f6bdc2
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:10:13 -0700

    Add 09. Vowels Sum Problem

---
+++ Programing basics C#/07. For-Loop/09. Vowels Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _09.Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            int sumOfPositions = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == 'a' || inputString[i] == 'A')
+                {
+                    sumOfPositions += 1;
+                }
+                else if (inputString[i] == 'e' || inputString[i] == 'E')
+                {
+                    sumOfPositions += 2;
+                }
+                else if (inputString[i] == 'i' || inputString[i] == 'I')
+                {
+                    sumOfPositions += 3;
+                }
+                else if (inputString[i] == 'o' || inputString[i] == 'O')
+                {
+                    sumOfPositions += 4;
+                }
+                else if (inputString[i] == 'u' || inputString[i] == 'U') {
+                    sumOfPositions += 5;
+                }
+            }
+            Console.WriteLine(sumOfPositions);
+        }
+    }
+}
+//A/a Ee Ii Oo Uu
