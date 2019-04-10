00420ea539551561bc02133087d55faaf8b38716
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:07:26 -0700

    Add 07. Left and Right Sum Problem

---
+++ Programing basics C#/07. For-Loop/07. Left and Right Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int sum1 = 0;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum1 += currentNum;
+            }
+
+            int sum2 = 0;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum2 += currentNum;
+            }
+
+            if (sum1 == sum2)
+                Console.WriteLine($"Yes, sum = {sum1}");
+            else
+                Console.WriteLine($"No, diff = {Math.Max(sum1, sum2) - Math.Min(sum1, sum2)}");
+        }
+    }
+}
