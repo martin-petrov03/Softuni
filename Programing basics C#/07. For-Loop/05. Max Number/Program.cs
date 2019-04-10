42b8b98ec6231e44bbc8c5a086062b4526ffd945
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:05:31 -0700

    Add 05. Max Number Problem

---
+++ Programing basics C#/07. For-Loop/05. Max Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if(currentNum > maxNum)
+                    maxNum = currentNum;
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
