45e1340af5ef5b69cc57dbec2a166232af4fdd34
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-10 11:06:31 -0700

    Add 06. Min Number Problem

---
+++ Programing basics C#/07. For-Loop/06. Min Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNums = int.Parse(Console.ReadLine());
+
+            int minNum = int.MaxValue;
+            for (int i = 1; i <= countNums; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum < minNum)
+                    minNum = currentNum;
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
