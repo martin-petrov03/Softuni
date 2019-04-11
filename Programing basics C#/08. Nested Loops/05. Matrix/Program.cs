1e1d36065840a66950f86b862ef80e6145df9c47
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 06:56:53 -0700

    Add 05. Matrix Problem

---
+++ Programing basics C#/08. Nested Loops/05. Matrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            for (int first = num1; first <= num2; first++)
+            {
+                for (int second = num1; second <= num2; second++)
+                {
+                    for (int third = num3; third <= num4; third++)
+                    {
+                        for (int fourth = num3; fourth <= num4; fourth++)
+                        {
+                            if (first != second && third != fourth)
+                            {
+                                if (first + fourth == third + second) {
+                                    Console.WriteLine($"{first}{second}");
+                                    Console.WriteLine($"{third}{fourth}");
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
+                    }
+                }                                  
+                    
+            }            
+        }
+    }
+}
