cb36055f71a096235aad262c911ae99b170492ed
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 06:59:05 -0700

    Add 06. Combination Problem

---
+++ Programing basics C#/08. Nested Loops/06. Combination/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06.Combination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = int.Parse(Console.ReadLine());
+
+            double counter = 0;
+            for (int a = 0; a <= result; a++)
+            {
+                for (int b = 0; b <= result; b++)
+                {
+                    for (int c = 0; c <= result; c++)
+                    {
+                        for (int d = 0; d <= result; d++)
+                        {
+                            for (int e = 0; e <= result; e++)
+                            {
+                                if (a + b + c + d + e == result)
+                                    counter++;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
