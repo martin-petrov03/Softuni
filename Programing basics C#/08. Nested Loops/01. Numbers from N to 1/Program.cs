66ee7209730dbaf8f213289cc4f1cdaaeea3463e
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 06:49:45 -0700

    Add 01. Numbers from N to 1 Problem

---
+++ Programing basics C#/08. Nested Loops/01. Numbers from N to 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.Numbers_from_N_to_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = num; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
