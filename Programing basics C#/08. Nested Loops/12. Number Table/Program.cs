244c3476907a0b2d0583a8b62c4b8f96af94a726
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 07:06:38 -0700

    Add 12. Number Table Problem

---
+++ Programing basics C#/08. Nested Loops/12. Number Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12.Number_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                int left = row;
+                int right = n - 1;
+                for (int col = 1; col <= n; col++)
+                {
+                    if (left <= n)
+                    {
+                        Console.Write(left + " ");
+                        left++;
+                    }
+                    else {
+                        Console.Write(right + " ");
+                        right--;
+                    }                                           
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
