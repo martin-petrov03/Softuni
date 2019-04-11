3206172544f7ffb1ca58de7ff8047d02f6de4823
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 06:53:06 -0700

    Add 03. Even Powers of 2 Problem

---
+++ Programing basics C#/08. Nested Loops/03. Even Powers of 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int powOfTwo = 0;
+            for (int i = 0; i <= num; i += 2)
+            {
+                powOfTwo = i;
+                Console.WriteLine(Math.Pow(2, powOfTwo));
+            }
+        }
+    }
+}
