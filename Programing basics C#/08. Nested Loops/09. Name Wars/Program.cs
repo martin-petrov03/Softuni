c50a1daa4d523111a734da1bba65bea7713342cc
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 07:03:10 -0700

    Add 09. Name Wars Problem

---
+++ Programing basics C#/08. Nested Loops/09. Name Wars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09.Name_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string bestName = string.Empty;
+            int bestScope = int.MinValue;
+            while (input != "STOP")
+            {                
+                int currentScope = 0;
+                string currentName = string.Empty;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    currentScope += (int)input[i];
+                    currentName += input[i];
+                }
+                if (currentScope > bestScope)
+                {
+                    bestScope = currentScope;
+                    bestName = currentName;
+                }
+                input = Console.ReadLine();
+            }
+                Console.WriteLine($"Winner is {bestName} - {bestScope}! ");
+        }
+    }
+}
