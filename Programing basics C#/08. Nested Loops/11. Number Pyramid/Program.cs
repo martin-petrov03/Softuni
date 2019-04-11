79f3cb80ec1a5e58892547bcd519f165665f520f
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 07:05:08 -0700

    Add 11. Number Pyramid Problem

---
+++ Programing basics C#/08. Nested Loops/11. Number Pyramid/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _11.Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+            int num = 1;
+
+            while (num <= n)
+            {
+                for (int row = 1; row <= counter; row++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                    if (num > n)
+                    {
+                        break;
+                    }
+                }
+                counter++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
