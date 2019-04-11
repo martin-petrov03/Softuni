f6e07e702ea2a58c1d463c24e4bb87ee87cea5ff
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 07:02:09 -0700

    Add 08. Travelling Problem

---
+++ Programing basics C#/08. Nested Loops/08. Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = "";
+
+            while ((destination = Console.ReadLine()) != "End") {
+                double prise = double.Parse(Console.ReadLine());
+
+                double savedMoneySum = 0;
+                double savedMoney = 0;
+                while (savedMoneySum < prise) {
+                    savedMoney = double.Parse(Console.ReadLine());
+                    savedMoneySum += savedMoney;
+                }
+                if(savedMoneySum >= prise)
+                    Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
