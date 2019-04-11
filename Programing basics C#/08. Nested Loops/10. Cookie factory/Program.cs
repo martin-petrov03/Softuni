4fcace2c3176ca29bb638e4581d22dceec0bf9dc
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-11 07:04:01 -0700

    Add 10. Cookie factory Problem

---
+++ Programing basics C#/08. Nested Loops/10. Cookie factory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _10.Cookie_factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countParts = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int countBakes = 0;
+                List<string> products = new List<string> { };
+            while (true)
+            {
+                
+                while (input != "Bake!") {
+
+                    products.Add(input);
+                    input = Console.ReadLine();
+                }
+
+                if (input == "Bake!")
+                {
+                    
+                    if (products.Contains("eggs") && products.Contains("sugar") && products.Contains("flour"))
+                        {
+                        countBakes++;
+                        Console.WriteLine($"Baking batch number {countBakes}...");
+                        products.Clear();
+                    }
+                        else {
+                            Console.WriteLine($"The batter should contain flour, eggs and sugar!");
+                        }
+                   
+                    if (countBakes >= countParts)
+                    {
+                        break;
+                    }
+                }               
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
