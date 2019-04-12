5b4514144519f647ad607d5ed4a45c81e00a61ba
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-12 10:11:30 -0700

    Add 03.Courier Express Problem

---
+++ Programing basics C#/Exams/Exam - 05 November 2017/03.Courier Express/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _03.Courier_Express
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = double.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            int distance = int.Parse(Console.ReadLine());
+
+            double priseForOneKm = 0;
+            if (type == "standard")
+            {
+                if (weight < 1)
+                {
+                    priseForOneKm = 3;
+                }
+                else if (weight >= 1 && weight <= 10)
+                {
+                    priseForOneKm = 5;
+                }
+                else if (weight >= 11 && weight <= 40)
+                {
+                    priseForOneKm = 10;
+                }
+                else if (weight >= 41 && weight <= 90)
+                {
+                    priseForOneKm = 15;
+                }
+                else if (weight >= 91)
+                {
+                    priseForOneKm = 20;
+                }
+                Console.WriteLine($"The delivery of your shipment with weight of {weight:F3} kg. would cost {(priseForOneKm / 100 * distance):F2} lv.");
+            }
+            else if (type == "express")
+            {
+                if (weight < 1)
+                {
+                    priseForOneKm = distance * 0.03 + 0.03 * 0.8 * weight * distance;
+                }
+                else if (weight >= 1 && weight <= 10)
+                {
+                    priseForOneKm = distance * 0.05 + 0.05 * 0.4 * weight * distance;
+                }
+                else if (weight >= 11 && weight <= 40)
+                {
+                    priseForOneKm = distance * 0.1 + 0.1 * 0.05 * weight * distance;
+                }
+                else if (weight >= 41 && weight <= 90)
+                {
+                    priseForOneKm = distance * 0.15 + 0.15 * 0.02 * weight * distance;
+                }
+                else if (weight >= 91)
+                {                   
+                    priseForOneKm = distance * 0.20 + 0.20 * 0.01 * weight * distance;
+                }
+    
+                Console.WriteLine($"The delivery of your shipment with weight of {weight:F3} kg. would cost {priseForOneKm:F2} lv.");
+            } else {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
