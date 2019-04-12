4f9e0dc71af879befafec8312010d23b62d2a1b2
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-12 10:14:15 -0700

    Add p06 Problem

---
+++ Programing basics C#/Exams/Exam - 05 November 2017/p06/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace p06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            if (n < 10)
+            {
+                i = n;
+            }
+            else {
+                i = 10;
+            }
+                for (int a = 1; a < i; a++)
+                {
+                    for (int b = 1; b < i; b++)
+                    {
+                        for (int c = 1; c < i; c++)
+                        {
+                            for (int d = 1; d < i; d++)
+                            {
+                               if ((a + b) == (c + d) && n % (a + b) == 0)
+                               {
+                                   Console.Write(a); Console.Write(b); Console.Write(c); Console.Write(d + " ");
+                               }
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
