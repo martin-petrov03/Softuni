588471a6e2af97187c633a03e12d34c1eff5b5b6
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-12 10:04:52 -0700

    Add 02.Lutenitsa Problem

---
+++ Programing basics C#/Exams/Exam - 05 November 2017/02.Lutenitsa/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02.Lutenitsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tomatoeKg = double.Parse(Console.ReadLine());
+            int countBoxes = int.Parse(Console.ReadLine());
+            int countJars = int.Parse(Console.ReadLine());
+
+            double totalKgLutenica = tomatoeKg / 5;
+            double jars = totalKgLutenica / 0.535;
+            double boxes = jars / countJars;
+            
+            if (boxes >= countBoxes) {
+                double remainingBoxes = boxes - countBoxes;
+                double remainingJars = jars - (countBoxes * countJars);
+                Console.WriteLine($"Total lutenica: {Math.Floor(totalKgLutenica)} kilograms.");
+                Console.WriteLine($"{Math.Floor(remainingBoxes)} boxes left.");               
+                Console.WriteLine($"{Math.Floor(remainingJars)} jars left.");
+            } else {
+                double remainingBoxes = countBoxes - boxes;
+                double remainingJars = (countBoxes * countJars) - jars;
+                Console.WriteLine($"Total lutenica: {Math.Floor(totalKgLutenica)} kilograms.");
+                Console.WriteLine($"{Math.Floor(remainingBoxes)} more boxes needed.");
+                Console.WriteLine($"{Math.Floor(remainingJars)} more jars needed.");
+            }
+        }
+    }
+}
