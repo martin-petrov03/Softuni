563fa22b1712e037d5c2a83f66563f98751fbefe
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-12 10:02:59 -0700

    Add 01.Wire Net Problem

---
+++ Programing basics C#/Exams/Exam - 05 November 2017/01.Wire Net/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Wire_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double priseForOneMeter = double.Parse(Console.ReadLine());
+            double weightForOneMeter = double.Parse(Console.ReadLine());
+
+            double perimeter = 2 * a + 2 * b;
+            double prise = perimeter * priseForOneMeter;
+            double area = h * perimeter;
+            double weight = area * weightForOneMeter;
+
+            Console.WriteLine($"{perimeter}");
+            Console.WriteLine($"{prise:F2}");           
+            Console.WriteLine($"{weight:F3}");
+        }
+    }
+}
