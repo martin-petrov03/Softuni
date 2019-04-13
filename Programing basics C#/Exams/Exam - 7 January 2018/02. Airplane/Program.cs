ce93344c449149cf992fbb0e4fe179174cf4e11e
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-13 13:32:41 -0700

    Add 02. Airplane Problem

---
+++ Programing basics C#/Exams/Exam - 7 January 2018/02. Airplane/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.Airplane
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            int duration = int.Parse(Console.ReadLine());
+
+            int minutesPlus = duration + minutes;
+            while (minutesPlus >= 60) {
+                hour += 1;
+                minutesPlus -= 60;
+            }
+            if (hour >= 24) {
+                hour = 0 + hour - 24;
+            }
+            Console.WriteLine($"{hour}h {minutesPlus}m");
+        }
+    }
+}
