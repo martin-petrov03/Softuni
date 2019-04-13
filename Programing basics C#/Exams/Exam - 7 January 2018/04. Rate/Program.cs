2480d5e2df7fe713e9a68cd89b6cae230cc96303
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-13 13:36:27 -0700

    Add 04. Rate Problem

---
+++ Programing basics C#/Exams/Exam - 7 January 2018/04. Rate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.Rate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double prise = double.Parse(Console.ReadLine());
+            int countMonths = int.Parse(Console.ReadLine());
+
+            double simpleInterest = 0;
+            double complexInterest = 0;
+
+            double interestPreviousMonth = prise;
+            for (int i = 1; i <= countMonths; i++)
+            {
+                simpleInterest += prise * 3 / 100;
+                complexInterest += interestPreviousMonth * 2.7 / 100.0;
+                interestPreviousMonth = complexInterest;
+                interestPreviousMonth += prise;
+            }
+            simpleInterest += prise;
+            complexInterest += prise;
+            double difference = Math.Max(simpleInterest, complexInterest) - Math.Min(complexInterest, simpleInterest);
+
+            Console.WriteLine($"Simple interest rate: {simpleInterest:F2} lv.");
+            Console.WriteLine($"Complex interest rate: {complexInterest:F2} lv.");
+            if(simpleInterest > complexInterest)
+                Console.WriteLine($"Choose a simple interest rate. You will win {difference:F2} lv.");
+            else
+                Console.WriteLine($"Choose a complex interest rate. You will win {difference:F2} lv.");
+        }
+    }
+}
