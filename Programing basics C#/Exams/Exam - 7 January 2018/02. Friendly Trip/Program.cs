47fa21f1e4b6e858cb7fa99204ba68bcaf276547
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-13 13:33:49 -0700

    Add 02. Friendly Trip Problem

---
+++ Programing basics C#/Exams/Exam - 7 January 2018/02. Friendly Trip/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.Friendly_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kilometers = double.Parse(Console.ReadLine());
+            int litersBenzinForHundertKm = int.Parse(Console.ReadLine());
+            double priseOneLiterBenzin = double.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double priseForBenzin = kilometers * litersBenzinForHundertKm / 100;
+            priseForBenzin *= priseOneLiterBenzin;
+         
+            
+            double difference = Math.Abs(priseForBenzin - budget); //разлика
+            double forEach = budget / 5.0; //допълнително от всеки
+
+            if (priseForBenzin <= budget)         
+                Console.WriteLine($"You can take a trip. {difference:F2} money left.");
+            else
+                Console.WriteLine($"Sorry, you cannot take a trip. Each will receive {forEach:F2} money.");
+        }
+    }
+}
