ac62f314fbbace716448e1f905b37e69944ed801
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-13 13:38:33 -0700

    Add 06. Coins Problem

---
+++ Programing basics C#/Exams/Exam - 7 January 2018/06. Coins/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06.Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal balance = decimal.Parse(Console.ReadLine());
+            // 2 / 1 / 0.50 / 0.20 / 0.10 / 0.05 / 0.02 / 0.01
+            int countTimes = 0;
+            while (balance >= 2) {
+                balance -= 2;
+                countTimes++;
+            }
+            while (balance >= 1)
+            {
+                balance -= 1;
+                countTimes++;
+            }
+            while (balance >= (decimal) 0.5)
+            {
+                balance -= 0.5m;
+                countTimes++;
+            }
+            while (balance >= 0.2m)
+            {
+                balance -= 0.2m;
+                countTimes++;
+            }
+            while (balance >= 0.1m)
+            {
+                balance -= 0.1m;
+                countTimes++;
+            }
+            while (balance >= 0.05m)
+            {
+                balance -= 0.05m;
+                countTimes++;
+            }
+            while (balance >= 0.02m)
+            {
+                balance -= 0.02m;
+                countTimes++;
+            }
+            while (balance >= 0.01m)
+            {
+                balance -= 0.01m;
+                countTimes++;
+            }
+            Console.WriteLine(countTimes);
+        }
+    }
+}
