62e615c47494409630dbb934431c037c06041d15
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-13 13:31:47 -0700

    Add 01. Sandbox Problem

---
+++ Programing basics C#/Exams/Exam - 7 January 2018/01. Sandbox/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double heigth = double.Parse(Console.ReadLine());
+            double priseOfKiloSand = double.Parse(Console.ReadLine());
+            double priseOfOneBoard = double.Parse(Console.ReadLine());
+            
+            double area = width * heigth;
+            double areaSandBox = (width - (0.2 + 0.2)) * (heigth - (0.2 + 0.2));
+            double areaCurbs = area - areaSandBox;
+            double quantitySand = Math.Ceiling(areaSandBox * 20);
+            double boards = Math.Ceiling(areaCurbs / (2.2 * 0.2));
+            double boardsPrise = priseOfOneBoard * boards;
+            double sandPrise = quantitySand * priseOfKiloSand;
+
+            double sum = boardsPrise + sandPrise;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
