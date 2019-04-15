e931b61e7a644935942af6e7943c5d5242dd9a14
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-15 12:10:15 -0700

    Add 06. Special Combinations Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam1/06. Special Combinations/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06.Special_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i <= a; i++)
+            {
+                for (int j = 2; j <= b; j++)
+                {
+                    for (int k = 2; k <= c; k++)
+                    {
+                        if ((i % 2 == 0) && isPrime(j) && (k % 2 == 0)) {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+                    }
+                }
+            }
+        }
+        static bool isPrime(int a) {
+            bool isPrime = true;
+            for (int i = a; i >= 1; i--)
+            {
+                if (i == a || i == 1)
+                {
+                    continue;
+                }
+                else if (a % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+    }
+}
+//start  -> 7:22
+//break ->   --->> 7:37
+//end    -> 
