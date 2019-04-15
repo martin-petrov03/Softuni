f611001fe292b9f3cf73854097bb98cbbfa1a7ec
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-15 12:08:45 -0700

    Add 02. Picture in the Wall Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam1/02. Picture in the Wall/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.Picture_in_the_Wall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int holeWidth = int.Parse(Console.ReadLine());
+            int holeHeigth = int.Parse(Console.ReadLine());
+            int picturesSides = int.Parse(Console.ReadLine());
+            int countPictures = int.Parse(Console.ReadLine());
+
+            int holeArea = holeWidth * holeHeigth;
+            int picturesArea = picturesSides * picturesSides * countPictures;
+            if(holeArea > picturesArea)
+                Console.WriteLine($"The pictures fit in the hole. Hole area is {holeArea - picturesArea} bigger than pictures area.");
+            else
+                Console.WriteLine($"The pictures don't fit in the hole. Picture area is {picturesArea - holeArea} bigger than hole area.");
+        }
+    }
+}
+//start  -> 22:03
+//end    -> 22:11
