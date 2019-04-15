3eab3525b774395c50cb6c4592f1aa10cd82fb0b
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-15 12:08:16 -0700

    Add 01. Hearthstone Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam1/01. Hearthstone/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.Hearthstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int breakMinutes = int.Parse(Console.ReadLine());
+            double packetCardsPrise = double.Parse(Console.ReadLine());
+            double adventurePrise = double.Parse(Console.ReadLine());
+            double coffeePrise = double.Parse(Console.ReadLine());
+
+            int breakMinutesWithoutCoffeTime = breakMinutes - 5;
+            int timeForCards = 3 * 2;
+            int timeForAdventure = 2 * 2;
+            
+            int remainingTime = breakMinutesWithoutCoffeTime - (timeForCards + timeForAdventure);
+            double moneyForPacks = 3 * packetCardsPrise;
+            double moneyForAdventure = 2 * adventurePrise;
+
+            double allSpentMoney = moneyForPacks + moneyForAdventure + coffeePrise;
+
+            Console.WriteLine($"{allSpentMoney:F2}");
+            Console.WriteLine($"{remainingTime}");
+        }
+    }
+}
+//start  -> 21:46
+//end    -> 22.03
