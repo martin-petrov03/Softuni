ddf0b0d064b705373b447beb997f1b0f7cb060f9
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-16 12:13:48 -0700

    Add 04. ASCII combinations Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam2/04. ASCII combinations/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace _4_CharsSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int maxSum = 0;
+
+            int sumDigits = 0;
+            int sumCapsLetters = 0;
+            int sumSmallLetters = 0;
+            int sumOthers = 0;
+
+            string digits = "";
+            string capsLetters = "";
+            string smallLetters = "";
+            string others = "";
+
+
+            for (int i = 0; i < num; i++)
+            {
+                char currSymbol = char.Parse(Console.ReadLine());
+
+                int currNum = (int)currSymbol;
+
+                if (currSymbol >= '0' && currSymbol <= '9')
+                {
+                    digits += currSymbol;
+                    sumDigits += currNum;
+
+                    if (sumDigits > maxSum)
+                    {
+                        maxSum = sumDigits;
+                    }
+
+                }
+                else if (currSymbol >= 'A' && currSymbol <= 'Z')
+                {
+                    capsLetters += currSymbol;
+                    sumCapsLetters += currNum;
+
+                    if (sumCapsLetters > maxSum)
+                    {
+                        maxSum = sumCapsLetters;
+                    }
+                }
+                else if (currSymbol >= 'a' && currSymbol <= 'z')
+                {
+                    smallLetters += currSymbol;
+                    sumSmallLetters += currNum;
+
+                    if (sumSmallLetters > maxSum)
+                    {
+                        maxSum = sumSmallLetters;
+                    }
+                }
+                else
+                {
+                    others += currSymbol;
+                    sumOthers += currNum;
+
+                    if (sumOthers > maxSum)
+                    {
+                        maxSum = sumOthers;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Biggest ASCII sum is:{maxSum}");
+
+            if (sumDigits == maxSum)
+            {
+                Console.WriteLine($"Combination of characters is:{digits}");
+            }
+            else if (sumCapsLetters == maxSum)
+            {
+                Console.WriteLine($"Combination of characters is:{capsLetters}");
+            }
+            else if (sumSmallLetters == maxSum)
+            {
+                Console.WriteLine($"Combination of characters is:{smallLetters}");
+            }
+            else
+            {
+                Console.WriteLine($"Combination of characters is:{others}");
+            }
+        }
+    }
+}
