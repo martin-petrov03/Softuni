319d95d746b4a45667ac0d3c34519f5cffad6bdf
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-16 12:10:01 -0700

    Add 02. Illidan Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam2/02. Illidan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02.Illidan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople = int.Parse(Console.ReadLine());
+            int powerOfOneOfAll = int.Parse(Console.ReadLine());
+            int healthOfIlindan = int.Parse(Console.ReadLine());
+
+            int totalPower = countPeople * powerOfOneOfAll;
+            if(totalPower > healthOfIlindan)
+                Console.WriteLine($"Illidan has been slain! You defeated him with {totalPower - healthOfIlindan} points.");
+            else if(totalPower < healthOfIlindan)
+                Console.WriteLine($"You are not prepared! You need {healthOfIlindan - totalPower} more points.");
+        }        
+    }
+}
+//start  ->  10:32
+//end  ->  10:38
