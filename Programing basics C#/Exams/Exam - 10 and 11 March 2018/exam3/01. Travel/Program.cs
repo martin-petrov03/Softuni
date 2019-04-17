574940a0e9796cd83b9b1a567071e89dfff96ca3
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-17 12:41:59 -0700

    Add 01. Travel Problem

---
+++ Programing basics C#/Exams/Exam - 10 and 11 March 2018/exam3/01. Travel/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aToBKm = double.Parse(Console.ReadLine());
+            double truckSpeed = double.Parse(Console.ReadLine());
+            double truckMinusAutoSpeed = double.Parse(Console.ReadLine());
+
+           double autoSpeed = truckSpeed + truckMinusAutoSpeed * 3.6;
+            //autoSpeed *= 3.6;
+
+            double truckTime = Math.Ceiling(aToBKm / truckSpeed);
+            double autoTime = Math.Ceiling(aToBKm / autoSpeed);
+            Console.WriteLine($"The truck arrived after {truckTime} hours");
+            Console.WriteLine($"The car arrived after {autoTime} hours");
+        }
+    }
+}
+// start -> 4:50
