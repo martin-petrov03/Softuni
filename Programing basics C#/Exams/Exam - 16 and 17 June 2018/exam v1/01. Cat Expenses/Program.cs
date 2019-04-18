bd8e0ab7451a519ec81298cedcee089e85897f78
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-18 12:18:58 -0700

    Add 01. Cat Expenses Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v1/01. Cat Expenses/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Cat_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bedPrise = double.Parse(Console.ReadLine());
+            double toiletPrise = double.Parse(Console.ReadLine());
+
+            double foodPrise = toiletPrise + toiletPrise * 25 / 100;
+            double playsPrise = foodPrise - foodPrise * 50 / 100;
+            double doctorPrise = playsPrise + playsPrise * 10 / 100;
+            double totalCostsForMonth = foodPrise + playsPrise + doctorPrise + toiletPrise;
+            double otherCosts = totalCostsForMonth * 5 / 100;
+            totalCostsForMonth += otherCosts;
+
+            double totalPrise = 12 * totalCostsForMonth + bedPrise;
+            Console.WriteLine($"{totalPrise:F2} lv.");
+        }   
+    }
+}
