7a4e0d06aaaf97a603f59a824407ed9d47e8726a
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-18 12:19:52 -0700

    Add 02. Cat Shirt Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v1/02. Cat Shirt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _02._Cat_Shirt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sleeveLength = double.Parse(Console.ReadLine());
+            double theFrontLength = double.Parse(Console.ReadLine());
+            string fabricType = Console.ReadLine();
+            string tie = Console.ReadLine();
+
+            double size = theFrontLength * 2 + sleeveLength * 2;
+            size += size * 0.1;
+
+            double size1 = size / 100;
+            
+            double price = 0;
+            if (fabricType == "Linen") {
+                price = 15;
+            }
+            else if (fabricType == "Cotton")
+            {
+                price = 12;
+            }
+            else if (fabricType == "Denim")
+            {
+                price = 20;
+            }
+            else if (fabricType == "Twill")
+            {
+                price = 16;
+            }
+            else if (fabricType == "Flannel")
+            {
+                price = 11;
+            }
+            double totalPrice = price * size1 + 10;
+            
+
+            if (tie == "Yes") {
+                totalPrice += totalPrice * 0.2;
+            }
+            
+            Console.WriteLine($"The price of the shirt is: {totalPrice:F2}lv.");
+        }
+    }
+}
