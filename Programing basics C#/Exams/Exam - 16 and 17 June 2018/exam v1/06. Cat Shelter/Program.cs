5d61012cbb66c34ef7b9e0e70a5479f4175dc908
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-18 12:22:33 -0700

    Add 06. Cat Shelter Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v1/06. Cat Shelter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06._Cat_Shelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodQualityKg = double.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            double totalKg = 0;
+            while (true) {
+                command = Console.ReadLine();
+                double kgPerEat = 0;
+                if (command == "Adopted") {
+                    if (foodQualityKg >= totalKg) {
+                        Console.WriteLine($"Food is enough! Leftovers: {(foodQualityKg - totalKg) * 1000} grams.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Food is not enough. You need {(totalKg - foodQualityKg) * 1000} grams more.");
+                    }
+                    break;
+                }
+                else
+                {
+                    kgPerEat = double.Parse(command);
+                    kgPerEat /= 1000;
+                    totalKg += kgPerEat;
+                }
+            }
+        }
+    }
+}
