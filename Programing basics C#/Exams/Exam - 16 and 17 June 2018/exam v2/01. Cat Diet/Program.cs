fbaf3dcac67ce1bbd46c5ca37fdda9e3d613183d
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-20 10:31:03 -0700

    Add 01. Cat Diet Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v2/01. Cat Diet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Cat_Diet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prozentMaznini = int.Parse(Console.ReadLine());
+            int prozentProteini = int.Parse(Console.ReadLine());
+            int prozentVuglehidrati = int.Parse(Console.ReadLine());
+            int totalCallories = int.Parse(Console.ReadLine());
+            int prozentWater = int.Parse(Console.ReadLine());
+
+            double totaGrammslMaznini = (prozentMaznini / 100.0 * totalCallories) / 9;
+            double totalGrammsProteini = (prozentProteini / 100.0 * totalCallories) / 4;
+            double totalGrammsBuglehidrati = (prozentVuglehidrati / 100.0 * totalCallories) / 4;
+
+            double totalWight = totaGrammslMaznini + totalGrammsProteini + totalGrammsBuglehidrati;
+            double calloriesForOneKgFood = totalCallories / totalWight;
+            //calloriesForOneKgFood = (int)calloriesForOneKgFood;
+            double calloriesForOneGrFood = calloriesForOneKgFood * (1 - prozentWater / 100.0);
+            
+            Console.WriteLine($"{(calloriesForOneGrFood):F4}");
+        }
+    }
+}
