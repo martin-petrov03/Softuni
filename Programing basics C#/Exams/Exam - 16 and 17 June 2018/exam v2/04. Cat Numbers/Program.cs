695f5c7d3edfef97d0fb1683feec898cdd63f6d7
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-20 10:39:47 -0700

    Add 04. Cat Numbers Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v2/04. Cat Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Cat_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int catsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= catsCount; i++)
+            {
+                string firstName = Console.ReadLine();
+                string secondName = Console.ReadLine();               
+                int lastNumsOfBirthYear = int.Parse(Console.ReadLine());
+
+                string specialCode = $"{lastNumsOfBirthYear}{(int)firstName[0]}{(int)secondName[0]}{i}";
+                Console.WriteLine(specialCode);
+            }
+        }
+    }
+}
