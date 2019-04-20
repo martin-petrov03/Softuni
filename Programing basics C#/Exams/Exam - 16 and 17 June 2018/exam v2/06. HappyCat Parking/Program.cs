ea36baedfdfcad9775d9e4c3b17badbae3028da7
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-20 10:40:55 -0700

    Add 06. HappyCat Parking Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v2/06. HappyCat Parking/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._HappyCat_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countDays = int.Parse(Console.ReadLine());
+            int countHoursForEveryDay = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            for (int currentDay = 1; currentDay <= countDays; currentDay++)
+            {
+                double thisDaySum = 0;
+                for (int currentHour = 1; currentHour <= countHoursForEveryDay; currentHour++)
+                {
+                    if (currentDay % 2 == 0 && currentHour % 2 != 0)
+                    {
+                        thisDaySum += 2.5;
+                    }
+                    else if (currentDay % 2 != 0 && currentHour % 2 == 0)
+                    {
+                        thisDaySum += 1.25;
+                    }
+                    else {
+                        thisDaySum += 1;
+                    }
+                }
+                Console.WriteLine($"Day: {currentDay} - {thisDaySum:F2} leva");
+                sum += thisDaySum;
+            }
+            Console.WriteLine($"Total: {sum:F2} leva");
+        }
+    }
+}
