37bbdd3ee1a278a2330d67b15421c2969f06d14e
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-20 10:37:43 -0700

    Add 03. Cat Training Attendance Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v2/03. Cat Training Attendance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _03._Cat_Training_Attendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startHour = int.Parse(Console.ReadLine());
+            int hourOfArrival = int.Parse(Console.ReadLine());
+            int minutesOfArrival = int.Parse(Console.ReadLine());
+            string dayOfWeek = Console.ReadLine();
+
+            double bonusPoints = 0;
+            if (hourOfArrival >= startHour - 1 && hourOfArrival < startHour)
+            {
+                bonusPoints = 1.5;
+            }
+            else if (hourOfArrival == startHour && minutesOfArrival <= 30)
+            {
+                bonusPoints = 1;
+            }
+            else if ((hourOfArrival == startHour && minutesOfArrival > 30) || (hourOfArrival > startHour && hourOfArrival <= startHour + 4)) {
+                bonusPoints = 0.5;
+            }
+
+            if (dayOfWeek == "Monday" || dayOfWeek == "Wednesday" || dayOfWeek == "Friday")
+            {
+                bonusPoints += 0.6;
+            }
+            else if (dayOfWeek == "Tuesday" || dayOfWeek == "Thursday" || dayOfWeek == "Saturday") {
+                bonusPoints += 0.8;
+            }
+            else if (dayOfWeek == "Sunday")
+            {
+                bonusPoints += 2;
+            }
+            Console.WriteLine($"{bonusPoints:F2}");
+        }
+    }
+}
