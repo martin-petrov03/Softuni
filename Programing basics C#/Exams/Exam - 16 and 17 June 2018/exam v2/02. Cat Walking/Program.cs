adfc5bc749b48ffe7db9d634952bbaab09cf812b
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-20 10:35:38 -0700

    Add 02. Cat Walking Problem

---
+++ Programing basics C#/Exams/Exam - 16 and 17 June 2018/exam v2/02. Cat Walking/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Cat_Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesWalk = int.Parse(Console.ReadLine());
+            int countWalks = int.Parse(Console.ReadLine());
+            int collectedCalloriesForThisDay = int.Parse(Console.ReadLine());
+
+            int totalBurnedCallories = countWalks * minutesWalk * 5;
+
+            if (totalBurnedCallories >= 50 / 100.0 * collectedCalloriesForThisDay)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {totalBurnedCallories}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {totalBurnedCallories}.");
+            }
+        }
+    }
+}
