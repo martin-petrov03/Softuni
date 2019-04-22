d4cd40f3a6e72eb962abef1e616c4290c2a24159
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-22 07:03:39 -0700

    Add 01. Christmas Preparation Problem

---
+++ Programing basics C#/Exams/Exam - 16 December 2017/01. Christmas Preparation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.Christmas_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rollerPaperCount = int.Parse(Console.ReadLine());
+            int rollerFabrikCount = int.Parse(Console.ReadLine());
+            double glueLitersCount = double.Parse(Console.ReadLine());
+            int prozentReduction = int.Parse(Console.ReadLine());
+
+            double priseRollerPaperCount = rollerPaperCount * 5.8;
+            double priseRollerFabrikCount = rollerFabrikCount * 7.2;
+            double priseGlueLitersCount = glueLitersCount * 1.2;
+
+            double totalSum = priseRollerPaperCount + priseRollerFabrikCount + priseGlueLitersCount;
+            double reduction = totalSum * prozentReduction / 100;
+            totalSum -= reduction;
+            Console.WriteLine($"{totalSum:F3}");
+        }
+    }
+}
