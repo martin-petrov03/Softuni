9b1076b6f50e67c15148df1e4863518da2a043cb
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-24 08:12:25 -0700

    Add 02. New Years Eve Party Problem

---
+++ Programing basics C#/Exams/Exam - 17 December 2017/02. New Years Eve Party/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.New_Years_Eve_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int guestsCount = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double priseForGuests = guestsCount * 20;
+            if (priseForGuests < budget)
+            {
+                //double remainingMoney = budget - priseForGuests;
+                double fireworksPrise = (budget - priseForGuests) * 40 / 100;                
+                double donation = budget - (priseForGuests + fireworksPrise);
+                Console.WriteLine($"Yes! {Math.Round(fireworksPrise)} lv are for fireworks and {Math.Round(donation)} lv are for donation.");
+            }
+            else if (priseForGuests < budget) {                                                
+                Console.WriteLine($"Yes! {0} lv are for fireworks and {0} lv are for donation.");
+            }
+            else
+            {
+                Console.WriteLine($"They won't have enough money to pay the covert. They will need {Math.Abs(Math.Round(budget - priseForGuests))} lv more.");
+            }
+        }
+    }
+}
