ad4f5b590097f9e768b41040af6eb20d4acde276
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-24 08:11:52 -0700

    Add 01. Ivanovi's Family Problem

---
+++ Programing basics C#/Exams/Exam - 17 December 2017/01. Ivanovi's Family/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.Ivanovi_s_Family
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double firstChildPresentPrise = double.Parse(Console.ReadLine());
+            double secondChildPresentPrise = double.Parse(Console.ReadLine());
+            double thirdtChildPresentPrise = double.Parse(Console.ReadLine());
+
+            double sum = firstChildPresentPrise + secondChildPresentPrise + thirdtChildPresentPrise;
+            double totalSum = (budget - sum) - (budget - sum) * 10 / 100;
+            
+           // double totalSum = budget - sumWithoutTax;
+            Console.WriteLine($"{totalSum:F2}");
+        }
+    }
+}
