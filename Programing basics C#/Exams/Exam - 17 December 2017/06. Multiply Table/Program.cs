e37e3fff8fedf26db1d65f36cf595f6d9a6d4776
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-24 08:15:44 -0700

    Add 06. Multiply Table Problem

---
+++ Programing basics C#/Exams/Exam - 17 December 2017/06. Multiply Table/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.Multiply_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+                      
+            int firstNum = num % 10;
+            num /= 10;
+            int secondNum = num % 10;
+            num /= 10;
+            int thirdNum = num % 10;
+            num /= 10;
+            
+            for (int i = 1; i <= firstNum; i++)
+            {
+                for (int j = 1; j <= secondNum; j++)
+                {
+                    for (int n = 1; n <= thirdNum; n++)
+                    {
+                        Console.WriteLine($"{i} * {j} * {n} = {i * j * n};");
+                    }
+                }
+            }
+        }
+    }
+}
