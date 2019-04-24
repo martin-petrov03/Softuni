5a56ad8801ccc012d244757be30be42a2726fef7
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-24 08:13:24 -0700

    Add 04. Workout

---
+++ Programing basics C#/Exams/Exam - 17 December 2017/04. Workout/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04.Workout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countDays = int.Parse(Console.ReadLine());
+            double firstDayKilometers = double.Parse(Console.ReadLine());
+
+            double totalKilometers = firstDayKilometers;
+            double kilometersPreviosDay = firstDayKilometers;
+            for (int i = 1; i <= countDays; i++)
+            {
+                int prozentPlus = int.Parse(Console.ReadLine());               
+                double kilometersThisDay =  kilometersPreviosDay + kilometersPreviosDay * prozentPlus / 100;
+                //Console.WriteLine(kilometersThisDay);
+                totalKilometers += kilometersThisDay;
+                kilometersPreviosDay = kilometersThisDay;
+            }
+            
+            if (totalKilometers >= 1000)
+            {
+                Console.WriteLine($"You've done a great job running {Math.Ceiling(totalKilometers - 1000)} more kilometers!");
+            }
+            else {
+                Console.WriteLine($"Sorry Mrs. Ivanova, you need to run {Math.Ceiling(1000 - totalKilometers)} more kilometers");
+            }
+        }
+    }
+}
