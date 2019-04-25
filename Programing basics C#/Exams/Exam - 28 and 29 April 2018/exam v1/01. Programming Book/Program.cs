744bb996f1ee37135502b13e3e92bf9bdc727a70
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-25 10:49:31 -0700

    Add 01. Programming Book Problem

---
+++ Programing basics C#/Exams/Exam - 28 and 29 April 2018/exam v1/01. Programming Book/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Programming_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double onePagePrise = double.Parse(Console.ReadLine());
+            double coverPrise = double.Parse(Console.ReadLine());
+            int prozentDiscount = int.Parse(Console.ReadLine());
+            double disignPrise = double.Parse(Console.ReadLine()); 
+            int prozentTeamCost = int.Parse(Console.ReadLine());
+
+            double totalSum = 899 * onePagePrise + 2 * coverPrise;
+            totalSum -= totalSum * prozentDiscount / 100;
+            totalSum += disignPrise;
+            totalSum -= totalSum * prozentTeamCost / 100;
+            Console.WriteLine($"Avtonom should pay {totalSum:F2} BGN.");
+        }
+    }
+}
