55dbb55464d17064bf206eaea64ddb318c19b466
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-25 22:55:36 -0700

    Add 04. Running In The Park Problem

---
+++ Programing basics C#/Exams/Exam - 28 and 29 April 2018/exam v2/04. Running In The Park/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04._Running_In_The_Park
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+
+            int totalMinutes = 0;
+            double totalDistanceKilometers = 0;
+            int totalCallories = 0;
+
+            for (int i = 1; i <= daysCount; i++)
+            {
+                int minutes = int.Parse(Console.ReadLine());
+                double distance = double.Parse(Console.ReadLine());
+                string distanceType = Console.ReadLine();
+
+                int calorries = minutes * 20;
+                totalCallories += calorries;
+
+                totalMinutes += minutes;
+
+                if (distanceType == "m")
+                {
+                    totalDistanceKilometers += distance / 1000;
+                }
+                else if (distanceType == "km") {
+                    totalDistanceKilometers += distance;
+                }
+            }
+            Console.WriteLine($"He ran {totalDistanceKilometers:F2}km for {totalMinutes} minutes and burned {totalCallories} calories.");
+        }
+    }
+}
