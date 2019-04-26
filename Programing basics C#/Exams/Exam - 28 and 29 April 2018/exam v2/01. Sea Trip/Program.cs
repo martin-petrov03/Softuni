e82f293111cc2d572ddb787e541a4404151d77a9
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-25 22:50:23 -0700

    Add 01. Sea Trip Problem

---
+++ Programing basics C#/Exams/Exam - 28 and 29 April 2018/exam v2/01. Sea Trip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Sea_Trip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double foodPrise = double.Parse(Console.ReadLine());
+            double souvenirsPrise = double.Parse(Console.ReadLine());
+            double hotelPrise = double.Parse(Console.ReadLine());
+
+            double totalMoneyForHotel = (hotelPrise - hotelPrise * 0.1) + (hotelPrise - hotelPrise * 0.15) + (hotelPrise - hotelPrise * 0.2);
+            double moneyForCar = 54.39;
+            double totalPrise = foodPrise * 3 + souvenirsPrise * 3 + totalMoneyForHotel + moneyForCar;
+            Console.WriteLine($"Money needed: {totalPrise:F2}");
+        }
+    }
+}
