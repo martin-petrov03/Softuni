2ae40675312d92dade34aaef984287224b01d4af
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-25 22:52:44 -0700

    Add 02. Annual Salary Problem

---
+++ Programing basics C#/Exams/Exam - 28 and 29 April 2018/exam v2/02. Annual Salary/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Annual_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearsExperience = int.Parse(Console.ReadLine());
+            string developerType = Console.ReadLine();
+
+            double salary = 0;
+            if (developerType == "C# Developer")
+            {
+                salary = 5400;
+            }
+            else if (developerType == "Java Developer")
+            {
+                salary = 5700;
+            }
+            else if (developerType == "Front-End Web Developer")
+            {
+                salary = 4100;
+            }
+            else if (developerType == "UX / UI Designer")
+            {
+                salary = 3100;
+            }
+            else if (developerType == "Game Designer")
+            {
+                salary = 3600;
+            }
+
+            if (yearsExperience <= 5) {
+                salary = salary - salary * 65.8 / 100;
+            }
+            double yearSalary = salary * 12;
+            Console.WriteLine($"Total earned money: {yearSalary:F2} BGN");
+        }
+    }
+}
