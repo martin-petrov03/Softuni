2ed76fcd6191e756ae6721fbac75db5567a1445c
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-26 22:10:55 -0700

    Add 04. Bus Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v1/04. Bus/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople = int.Parse(Console.ReadLine());
+            int countStops = int.Parse(Console.ReadLine());
+
+            int totalPeople = countPeople;
+            for (int i = 1; i <= countStops; i++)
+            {
+                int descending = int.Parse(Console.ReadLine());
+                int rising = int.Parse(Console.ReadLine());
+
+                totalPeople -= descending;
+                totalPeople += rising;
+                if(i % 2 == 0)
+                {
+                    totalPeople -= 2;
+                }
+                else
+                {
+                    totalPeople += 2;
+                }
+            }
+            Console.WriteLine($"The final number of passengers is : {totalPeople}");
+        }
+    }
+}
