6332a24c4359882df288cf5d670654e3605714cd
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-26 22:11:39 -0700

    Add 06. Car Number Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v1/06. Car Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6._Car_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = int.Parse(Console.ReadLine());
+            int to = int.Parse(Console.ReadLine());
+
+            for (int num1 = from; num1 <= to; num1++)
+            {
+                for (int num2 = from; num2 <= to; num2++)
+                {
+                    for (int num3 = from; num3 <= to; num3++)
+                    {
+                        for (int num4 = from; num4 <= to; num4++)
+                        {
+                            if (((num2 + num3) % 2 == 0) && (num1 > num4)) {
+                                if ((num1 % 2 == 0 && num4 % 2 != 0) || (num1 % 2 != 0 && num4 % 2 == 0)) {
+                                    Console.Write($"{num1}{num2}{num3}{num4} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
