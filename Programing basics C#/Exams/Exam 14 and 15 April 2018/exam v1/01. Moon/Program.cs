90f1038c96398a478c4d03cf2076c9be71ef5145
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-26 22:03:50 -0700

    Add 01. Moon Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v1/01. Moon/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._Moon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance = 384400;
+            int hoursBreak = 3;
+
+            double speed = double.Parse(Console.ReadLine());
+            double fuelFor100Km = double.Parse(Console.ReadLine());
+
+            double totalTime = (384400 * 2) / speed;
+            totalTime = Math.Ceiling(totalTime);
+            totalTime += 3;
+
+            double totalFuel = (fuelFor100Km * 384400 * 2) / 100;
+
+            Console.WriteLine($"{totalTime:F0}");
+            Console.WriteLine($"{totalFuel}");
+        }
+    }
+}
