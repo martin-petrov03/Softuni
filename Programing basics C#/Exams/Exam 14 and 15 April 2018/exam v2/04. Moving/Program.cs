5b3b94fb731a59c0658119ceab5397484808750c
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-28 08:40:33 -0700

    Add 04. Moving Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v2/04. Moving/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _4._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeSpaceWidth = int.Parse(Console.ReadLine());
+            int freeSpaceLength = int.Parse(Console.ReadLine());
+            int freeSpaceheight = int.Parse(Console.ReadLine());
+            string command;
+
+            int freeSpaceVolume = freeSpaceheight * freeSpaceLength * freeSpaceWidth;
+            int boxVolume = 0;
+            bool notEnoughtSpace = false;
+
+            while (true)
+            {
+                command = Console.ReadLine();                
+                if (command == "Done") {
+                    break;
+                }
+                int currentCountBoxes = int.Parse(command);
+                boxVolume += currentCountBoxes;
+                if (boxVolume > freeSpaceVolume)
+                {
+                    notEnoughtSpace = true;
+                    break;
+                }
+            }
+
+            if (notEnoughtSpace)
+            {
+                Console.WriteLine($"No more free space! You need {boxVolume - freeSpaceVolume} Cubic meters more.");
+            }
+            else {
+                Console.WriteLine($"{freeSpaceVolume - boxVolume} Cubic meters left.");
+            }
+        }
+    }
+}
