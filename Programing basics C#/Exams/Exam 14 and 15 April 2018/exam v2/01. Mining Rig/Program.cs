e9be7b0359ba9740647aecc38780e3ce2884676e
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-28 08:37:47 -0700

    Add 01. Mining Rig Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v2/01. Mining Rig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1._Mining_Rig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int videoChipPrise = int.Parse(Console.ReadLine());
+            int adapterPrise = int.Parse(Console.ReadLine());
+            double priseCurrentForOneCard = double.Parse(Console.ReadLine());
+            double winOfOneCardForDay = double.Parse(Console.ReadLine());
+
+            double totalSum = videoChipPrise * 13 + adapterPrise * 13 + 1000;
+            double winForOneChip = winOfOneCardForDay - priseCurrentForOneCard;
+            double totalWinForDay = winForOneChip * 13;
+
+            Console.WriteLine(totalSum);
+            Console.WriteLine((int)Math.Ceiling(totalSum / totalWinForDay));
+        }
+    }
+}
