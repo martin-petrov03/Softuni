0e95b23d49553c3ee8c538477bcf9b8e4f03a697
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-28 08:39:10 -0700

    Add 02. AND Processors Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v2/02. AND Processors/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2._AND_Processors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal cpuCount = decimal.Parse(Console.ReadLine());
+            int staffsCount = int.Parse(Console.ReadLine());
+            int workedDays = int.Parse(Console.ReadLine());
+
+            int hoursWork = 8 * workedDays * staffsCount;
+            int makedCpuCOunt = hoursWork / 3;
+            if (makedCpuCOunt >= cpuCount)
+            {
+                double profits = (makedCpuCOunt - (double)cpuCount) * 110.10;
+                Console.WriteLine($"Profit: -> {profits:F2} BGN");
+            }
+            else {
+                double losts = ((double)cpuCount - makedCpuCOunt) * 110.10;                
+                Console.WriteLine($"Losses: -> {losts:F2} BGN");
+            }
+        }
+    }
+}
