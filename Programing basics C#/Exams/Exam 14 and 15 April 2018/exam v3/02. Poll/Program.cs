ed030b62b606d9adba382a0e429291db51ed36ef
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-29 10:31:24 -0700

    Add 02. Poll Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v3/02. Poll/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2._Poll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int votesForFirstLanguages = int.Parse(Console.ReadLine());
+
+            double votesForSecondLanguages = votesForFirstLanguages - votesForFirstLanguages * 20 / 100d;
+            double votesForThirdLanguages = votesForSecondLanguages - votesForSecondLanguages * 10 / 100d;
+
+            double votesForFirstThreeLangugages = votesForFirstLanguages + votesForSecondLanguages + votesForThirdLanguages;
+
+            if (votesForFirstThreeLangugages >= people / 2)
+            {
+                Console.WriteLine($"First three languages have {Math.Ceiling(votesForFirstThreeLangugages - people / 2)} votes more");
+            }
+            else {
+                Console.WriteLine($"First three languages have {Math.Ceiling(people / 2 - votesForFirstThreeLangugages)} votes less of half votes");
+            }
+
+        }
+    }
+}
