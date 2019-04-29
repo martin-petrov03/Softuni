e691bef2fd98e9b965826a231549c3184a6137a5
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-29 10:59:28 -0700

    Add 04. Grandpa Stavri Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v3/04. Grandpa Stavri/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.Grandpa_Stavri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+
+            double totalSum = 0;
+            double totalLiters = 0;
+            double previouslyDaySum = 0;
+            for (int i = 1; i <= daysCount; i++)
+            {
+                double qualityRaki = double.Parse(Console.ReadLine());
+                double currentRakiGradus = double.Parse(Console.ReadLine());
+                totalLiters += qualityRaki;
+                totalSum = previouslyDaySum + qualityRaki * currentRakiGradus;
+                previouslyDaySum = previouslyDaySum + qualityRaki * currentRakiGradus;
+            }
+
+            double degrees = totalSum / totalLiters;
+            Console.WriteLine($"Liter: {totalLiters:F2}");
+            Console.WriteLine($"Degrees: {degrees:F2}");
+            if (degrees < 38)
+            {
+                Console.WriteLine("Not good, you should baking!");
+            }
+            else if (degrees >= 38 && degrees <= 42)
+            {
+                Console.WriteLine("Super!");
+            }
+            else {
+
+                Console.WriteLine("Dilution with distilled water!");
+            }
+        }
+    }
+}
