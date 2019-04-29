0b887f31b078b316f4699264df905290f473e074
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-29 10:30:52 -0700

    Add 01. Cherry Jars Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v3/01. Cherry Jars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1._Cherry_Jars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kompotCount = int.Parse(Console.ReadLine());
+            int marmaladeCount = int.Parse(Console.ReadLine());
+
+            kompotCount += 1;
+            marmaladeCount += 1;
+
+            double qualityCherryForKompot = (kompotCount * 0.300) + (kompotCount * 0.300) * 5 / 100;
+            double qualityCherryForMarmalade = (marmaladeCount * 0.650) + (marmaladeCount * 0.650) * 10 / 100;
+
+            double totalQuality = qualityCherryForKompot + qualityCherryForMarmalade;
+            double totalCherryPrise = totalQuality * 3.2;
+            Console.WriteLine($"{totalCherryPrise:F2}");
+
+        }
+    }
+}
