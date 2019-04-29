a0fa3817d41da6e9f7d7a7f3fa86781df23d65ff
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-29 11:00:37 -0700

    Add 06. Bitcoin Code Generator Problem

---
+++ Programing basics C#/Exams/Exam 14 and 15 April 2018/exam v3/06. Bitcoin Code Generator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _6._Bitcoin_Code_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxCount = int.Parse(Console.ReadLine());
+
+            char i = (char)32;
+            char j = (char)57;
+
+            int count = 0;
+            for (int k = 1; k <= a; k++)
+            {
+                for (int l = 1; l <= b; l++)
+                {
+                    i++;
+                    j++;
+                    if (i > 47) i = (char)33;
+                    if (j > 64) j = (char)58;
+                    count++;
+                    //System.out.printf("%s%s%s%s%s%s|", i, j, k, l, j, i);
+                    Console.Write($"{i}{j}{k}{l}{j}{i}|");
+                    if (count == maxCount) return;
+
+                }
+            }
+
+        }
+    }
+}
