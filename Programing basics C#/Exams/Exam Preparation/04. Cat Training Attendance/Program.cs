a067e691ea0b1cbd7a19688da049c60e15887f12
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:17:48 -0700

    Add 04. Cat Training Attendance Problem

---
+++ Programing basics C#/Exams/Exam Preparation/04. Cat Training Attendance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04._Cat_Training_Attendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int boughtFoodKg = int.Parse(Console.ReadLine());
+            string command = string.Empty;
+
+            int boughtFoodGramms = boughtFoodKg * 1000;
+
+            int totalGrammsAteFood = 0;
+            while ((command = Console.ReadLine()) != "Adopted") {
+                int grammsFood = int.Parse(command);
+                totalGrammsAteFood += grammsFood;
+            }
+
+            if (totalGrammsAteFood <= boughtFoodGramms)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {(boughtFoodGramms - totalGrammsAteFood)} grams.");
+            }
+            else {
+                Console.WriteLine($"Food is not enough. You need {(totalGrammsAteFood - boughtFoodGramms)} grams more.");
+            }
+        }
+    }
+}
