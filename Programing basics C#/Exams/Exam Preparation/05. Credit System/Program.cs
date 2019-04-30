cd3c8444a69c3b8d523063b5599f019eae9e6dd7
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:18:22 -0700

    Add 05. Credit System Problem

---
+++ Programing basics C#/Exams/Exam Preparation/05. Credit System/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Credit_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kursesCount = int.Parse(Console.ReadLine());
+
+            double gradesSum = 0;
+            double creditsSum = 0;
+            for (int i = 1; i <= kursesCount; i++)
+            {
+                int creditsAndGrade = int.Parse(Console.ReadLine());
+
+                string ex = creditsAndGrade.ToString();
+
+                char c = ex.Last();
+                double grade = double.Parse(c.ToString());
+                
+                gradesSum += grade;         
+                double maxCredits = double.Parse(ex.TrimEnd(c));
+
+                double currentCredits = 0;
+                if (grade == 3) {
+                    currentCredits = maxCredits / 2;
+                }
+                else if (grade == 4)
+                {
+                    currentCredits = maxCredits * 70 /100;
+                }
+                else if (grade == 5)
+                {
+                    currentCredits = maxCredits * 85 / 100;
+                }
+                else if (grade == 6)
+                {
+                    currentCredits = maxCredits;
+                }
+                creditsSum += currentCredits;
+            }
+            Console.WriteLine($"{creditsSum:F2}");
+            Console.WriteLine($"{(gradesSum / kursesCount):F2}");
+        }
+    }
+}
