0cde8d3cf2cd929b7288ae24deb3ab30749a957a
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:14:46 -0700

    Add 01. PC Store Problem

---
+++ Programing basics C#/Exams/Exam Preparation/01. PC Store/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._PC_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 - 1.57
+            double cpuDollarsPrise = double.Parse(Console.ReadLine());
+            double videoChipDollarsPrise = double.Parse(Console.ReadLine());
+            double ramDollarsPrise = double.Parse(Console.ReadLine());
+            int ramsCount = int.Parse(Console.ReadLine());
+            double prozents = double.Parse(Console.ReadLine());
+
+            double cpuPriseLeva = cpuDollarsPrise * 1.57;
+            double videoChipPriseLeva = videoChipDollarsPrise * 1.57;
+            double ramPriseLeva = ramDollarsPrise * ramsCount * 1.57;
+
+            cpuPriseLeva -= cpuPriseLeva * prozents;
+            videoChipPriseLeva -= videoChipPriseLeva * prozents;
+
+            double totalSum = cpuPriseLeva + videoChipPriseLeva + ramPriseLeva;
+            Console.WriteLine($"Money needed - {totalSum:F2} leva.");
+        }
+    }
+}
