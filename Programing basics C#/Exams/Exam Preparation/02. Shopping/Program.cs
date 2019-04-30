8350e980e8ff8c974def72d8f2c510e5a9e94311
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:16:26 -0700

    Add 02. Shopping Problem

---
+++ Programing basics C#/Exams/Exam Preparation/02. Shopping/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02._Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int countChocolates = int.Parse(Console.ReadLine());
+            double milkQuality = double.Parse(Console.ReadLine());
+
+            double chocolatePrise = countChocolates * 0.65;
+            double milkPrise = milkQuality * 2.7;
+            int mandariniCount = (int)Math.Floor(countChocolates - (countChocolates * 0.35));
+            double mandariniPrise = mandariniCount * 0.2;
+
+            double totalSum = chocolatePrise + milkPrise + mandariniPrise;
+
+            if (totalSum <= budget) {
+                Console.WriteLine($"You got this, {(budget - totalSum):F2} money left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {(totalSum - budget):F2} more!");
+            }
+        }
+    }
+}
