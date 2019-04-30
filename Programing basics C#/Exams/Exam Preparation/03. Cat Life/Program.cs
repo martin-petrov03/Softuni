8aa6e30e66486d65b72a57608d86a34cd5e71bc7
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:17:01 -0700

    Add 03. Cat Life Problem

---
+++ Programing basics C#/Exams/Exam Preparation/03. Cat Life/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _03._Cat_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string breed = Console.ReadLine();
+            char sex = char.Parse(Console.ReadLine());
+
+            bool isInvalidBreed = false;
+            int catsMonthsLife = 2;
+            if (sex == 'm')
+            {
+                if (breed == "British Shorthair")
+                {
+                    catsMonthsLife *= 13;
+                }
+                else if (breed == "Siamese")
+                {
+                    catsMonthsLife *= 15;
+                }
+                else if (breed == "Persian")
+                {
+                    catsMonthsLife *= 14;
+                }
+                else if (breed == "Ragdoll")
+                {
+                    catsMonthsLife *= 16;
+                }
+                else if (breed == "American Shorthair")
+                {
+                    catsMonthsLife *= 12;
+                }
+                else if (breed == "Siberian")
+                {
+                    catsMonthsLife *= 11;
+                }
+                else
+                {
+                    isInvalidBreed = true;
+                }
+            }
+            else if (sex == 'f')
+            {
+                if (breed == "British Shorthair")
+                {
+                    catsMonthsLife *= 14;
+                }
+                else if (breed == "Siamese")
+                {
+                    catsMonthsLife *= 16;
+                }
+                else if (breed == "Persian")
+                {
+                    catsMonthsLife *= 15;
+                }
+                else if (breed == "Ragdoll")
+                {
+                    catsMonthsLife *= 17;
+                }
+                else if (breed == "American Shorthair")
+                {
+                    catsMonthsLife *= 13;
+                }
+                else if (breed == "Siberian")
+                {
+                    catsMonthsLife *= 12;
+                }
+                else
+                {
+                    isInvalidBreed = true;
+                }
+            }
+            else
+            {
+                isInvalidBreed = true;
+            }            
+
+            if (isInvalidBreed)
+            {
+                Console.WriteLine($"{breed} is invalid cat!");
+            }
+            else
+            {
+                Console.WriteLine($"{catsMonthsLife} cat months");
+            }
+        }
+    }
+}
