795e1b7ffaf878be3a3c53e2e4a56c750f9de470
martinpetrov000 <martin.petrov033@gmail.com>
2019-04-30 10:18:54 -0700

    Add 06. Letters Combinations Problem

---
+++ Programing basics C#/Exams/Exam Preparation/06. Letters Combinations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._Letters_Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            int counter = 0;
+            for (char i = a; i <= b; i++)
+            {
+                for (char j = a; j <= b; j++)
+                {
+                    for (char k = a; k <= b; k++)
+                    {
+                        if (i != c && j != c && k != c)
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.Write("{0}", counter);
+        }
+    }
+}
