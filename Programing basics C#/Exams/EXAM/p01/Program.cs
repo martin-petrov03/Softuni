be695542e7f09f9a28afbdf8fbd9a71e81bf230b
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-01 10:25:29 -0700

    Add first Problem

---
+++ Programing basics C#/Exams/EXAM/p01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace p01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countSectors = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            double ticketPrise = double.Parse(Console.ReadLine());
+
+            double moneyForOneSector = (capacity * ticketPrise) / countSectors;
+            double sumAllSectors = moneyForOneSector * countSectors;
+            double charity = (sumAllSectors - (moneyForOneSector * 0.75)) / 8;
+
+            Console.WriteLine($"Total income - {sumAllSectors:F2} BGN");
+            Console.WriteLine($"Money for charity - {charity:F2} BGN");
+        } 
+    }
+}
