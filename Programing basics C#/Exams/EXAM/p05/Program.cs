3ad12aa144d750359279a2629c3551ff74dc4531
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-01 10:29:50 -0700

    Add fifth Problem

---
+++ Programing basics C#/Exams/EXAM/p05/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace p05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string team = Console.ReadLine();
+            int countMatches = int.Parse(Console.ReadLine());
+
+            int minutesAllMatchesSum = 0;
+            int countMatchesWithPenalties = 0;
+            int countMatchesWithAdditionalTime = 0;
+            for (int i = 1; i <= countMatches; i++)
+            {
+                int minutesTime = int.Parse(Console.ReadLine());
+                minutesAllMatchesSum += minutesTime;
+                if (minutesTime > 90) {
+                    countMatchesWithAdditionalTime++;
+                    if (minutesTime > 120) {
+                        countMatchesWithPenalties++;
+                    }
+                }
+            }
+            double average = (double)minutesAllMatchesSum / countMatches;
+            Console.WriteLine($"{team} has played {minutesAllMatchesSum} minutes. Average minutes per game: {average:F2}");
+            Console.WriteLine($"Games with penalties: {countMatchesWithPenalties}");
+            Console.WriteLine($"Games with additional time: {countMatchesWithAdditionalTime - countMatchesWithPenalties}");
+        }
+    }
+}
