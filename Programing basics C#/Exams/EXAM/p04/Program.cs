6066a4b123b1fdf8f6b70dca0c1dd78742bfb298
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-01 10:28:38 -0700

    Add fourth Problem

---
+++ Programing basics C#/Exams/EXAM/p04/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace p04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string team = Console.ReadLine();
+            int countMatches = int.Parse(Console.ReadLine());
+
+            int sumTotalPoints = 0;
+            int scoredGoalsSum = 0;
+            int havenGoalsSum = 0;
+            int goalsDifferenceSums = 0;
+            for (int i = 1; i <= countMatches; i++)
+            {
+                int scoreGoals = int.Parse(Console.ReadLine());
+                int havenGoals = int.Parse(Console.ReadLine());
+
+                scoredGoalsSum += scoreGoals;
+                havenGoalsSum += havenGoals;
+
+                int currentPoints = 0;
+                if (scoreGoals > havenGoals)
+                {
+                    currentPoints = 3;
+                    goalsDifferenceSums += scoreGoals - havenGoals;
+                }
+                else if (scoreGoals == havenGoals)
+                {
+                    currentPoints = 1;
+                    goalsDifferenceSums += scoreGoals - havenGoals;
+                }
+                else {
+                    currentPoints = 0;
+                    goalsDifferenceSums += scoreGoals - havenGoals;
+                }             
+                sumTotalPoints += currentPoints;
+            }
+
+            if (scoredGoalsSum >= havenGoalsSum)
+            {
+                Console.WriteLine($"{team} has finished the group phase with {sumTotalPoints} points.");
+                Console.WriteLine($"Goal difference: {goalsDifferenceSums}.");
+            }
+            else {
+                Console.WriteLine($"{team} has been eliminated from the group phase.");
+                Console.WriteLine($"Goal difference: {goalsDifferenceSums}.");
+            }
+        }
+    }
+}
