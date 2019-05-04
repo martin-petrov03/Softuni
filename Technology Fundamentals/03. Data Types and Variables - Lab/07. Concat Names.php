b44fd2b330e51b0a4520f29681ec70c6043d8df1
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-04 11:16:47 -0700

    Add 07. Concat Names.php Problem

---
+++ Technology Fundamentals/03. Data Types and Variables - Lab/07. Concat Names.php
@@ -0,0 +1,8 @@
+<?php
+    $fisrt_name = readline();
+    $secondt_name = readline();
+    $delimiter = readline();
+    
+    $output_string = $fisrt_name . $delimiter . $secondt_name;
+    echo $output_string;
+?>
