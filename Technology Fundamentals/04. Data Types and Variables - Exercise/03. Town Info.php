08e634e740b76cf0f21ed0f81547bb49ef178a3e
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-05 10:05:29 -0700

    Add 03. Town Info.php Problem

---
+++ Technology Fundamentals/04. Data Types and Variables - Exercise/03. Town Info.php
@@ -0,0 +1,8 @@
+<?php
+    $town_name = readline();
+    $population = intval(readline());
+    $area = intval(readline());
+    
+    $output_string = "Town $town_name has population of $population and area $area square km.";
+    echo $output_string;
+?>        
