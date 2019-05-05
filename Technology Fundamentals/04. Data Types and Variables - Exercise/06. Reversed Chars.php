3e092338f36b7cd2dd7e67df1970f18df857c391
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-05 10:08:06 -0700

    Add 06. Reversed Chars.php Problem

---
+++ Technology Fundamentals/04. Data Types and Variables - Exercise/06. Reversed Chars.php
@@ -0,0 +1,8 @@
+<?php
+    $first_string = readline();
+    $second_string = readline();
+    $third_string = readline();
+      
+    $output_string = $first_string . " " . $second_string . " " . $third_string;
+    echo strrev($output_string);
+?>
