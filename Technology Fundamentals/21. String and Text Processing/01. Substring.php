f2594af9ce277c69cfc0a9e20c95733bb3dc2eaf
martinpetrov000 <martin.petrov033@gmail.com>
2019-05-27 06:55:30 +0300

    Add 01. Substring Problem

---
+++ Technology Fundamentals/21. String and Text Processing/01. Substring.php
@@ -0,0 +1,8 @@
+<?php
+    $text1 = readline();
+    $text2 = readline();
+    
+    $text2 = str_replace($text1, '', $text2);
+    $text2 = str_replace($text1, '', $text2);    
+    print($text2);
+?>
