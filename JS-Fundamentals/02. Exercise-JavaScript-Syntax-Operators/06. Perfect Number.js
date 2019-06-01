9335c0107c4ae0733284210badab59fed5c9eddd
Martin Miroslavov Petrov <martin.petrov033@gmail.com>
2019-06-01 22:54:00 +0300

    Modify 06. Perfect Number Problem

--- JS-Fundamentals/02. Exercise-JavaScript-Syntax-Operators/06. Perfect Number.js
+++ JS-Fundamentals/02. Exercise-JavaScript-Syntax-Operators/06. Perfect Number.js
@@ -1,4 +1,14 @@
-function solve(){
-    console.log('a')
+function solve(perfectNum, inputNums){
+    let contains = false;
+    inputNums.forEach((num) => {
+        if(num === perfectNum){
+            contains = true;
+        }
+    });
+	if(contains){
+		console.log('There is perfect num in array');
+	}else{
+		console.log('There is not perfect num in array');
+	}
 }
-solve(['5', '6', '28']);
+solve(5, [5, 6, 28]);
