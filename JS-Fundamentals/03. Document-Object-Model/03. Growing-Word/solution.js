9ac686e74dd1acb01db2a1482fc3842a79ed8c42
martinpetrov000 <martin.petrov033@gmail.com>
2019-06-04 16:32:05 +0300

    Add js file

---
+++ JS-Fundamentals/03. Document-Object-Model/03. Growing-Word/solution.js
@@ -0,0 +1,13 @@
+function solve() {
+   let pElement = querySelector('.color-text');
+   console.log(pElement);
+   pElement.style.font-size == 5;
+   pElement.style.color = "red";
+   /*var elements = document.getElementsByClassName('color-text');
+
+      for (let i = 0; i < elements.length; i++) {
+      let element = elements[i];
+      element.style.fontSize = "2em";
+      }*/
+   document.document.getElementsByClassName('color-text').style.color = "red";
+}
