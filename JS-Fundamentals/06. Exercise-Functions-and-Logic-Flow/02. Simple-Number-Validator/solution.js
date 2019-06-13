92426e0dda101635a8c1f5a730399393ec3be9e6
martinpetrov000 <martin.petrov033@gmail.com>
2019-06-13 08:53:57 +0300

    Modify js file

--- JS-Fundamentals/06. Exercise-Functions-and-Logic-Flow/02. Simple-Number-Validator/solution.js
+++ JS-Fundamentals/06. Exercise-Functions-and-Logic-Flow/02. Simple-Number-Validator/solution.js
@@ -1,3 +1,17 @@
+document.querySelector('#check').addEventListener("click", validate);
 function validate() {
-    
+	console.log('12');
+    const num = +document.querySelector('#input').value;
+    let message = '';
+    if(num < 0){
+    	message = 'Negative Number';
+    } else if(num === 0){
+    	message = "Zero";
+    } else if(num > 0){
+    	message = "Positive Number";
+    }else{
+    	message = "Invalid Input";
+    }
+    message += '!';
+    document.querySelector('#response').textContent = message;
 }
