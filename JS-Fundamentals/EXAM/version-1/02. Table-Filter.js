e40e21ccae92bfc2d568fa903278169152c5816c
Martin Miroslavov Petrov <martin.petrov033@gmail.com>
2019-07-02 21:05:25 +0300

    Update 02. Table-Filter.js

--- JS-Fundamentals/EXAM/version-1/02. Table-Filter.js
+++ JS-Fundamentals/EXAM/version-1/02. Table-Filter.js
@@ -1,16 +1,17 @@
-function solve(inputArr, sort){
+function solve(inputArr){
     let outputArr = [];
-    for(let arr of inputArr){
-        outputArr = [];
-        for(let element of arr){
-            outputArr.push(element);
-        }
-        console.log(outputArr.join(' | '));
-    }
+    for(let i = 1; i < inputArr.length; i++){		
+        let name = inputArr[i][0];
+        let age = inputArr[i][1];
+		let grade = inputArr[i][2];
+		outputArr.push([name, age, grade]);        
+	}	
+	outputArr.forEach((arr) => {
+		console.log(arr.join(' | '));
+	});
 }
 solve([['name', 'age', 'grade'],
 ['Peter', '25', '5.00'],
 ['George', '34', '6.00'],
-['Marry', '28', '5.49']],
-'sort name'
+['Marry', '28', '5.49']]
 );
