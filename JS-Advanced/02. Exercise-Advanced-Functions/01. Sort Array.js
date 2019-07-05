e923a9c94d30bd1f6dcd9ebdd3878005cbb38421
Martin Miroslavov Petrov <martin.petrov033@gmail.com>
2019-07-05 20:05:01 +0300

    Delete 01. Sort Array.js

--- JS-Advanced/02. Exercise-Advanced-Functions/01. Sort Array.js
+++
@@ -1,9 +0,0 @@
-function solve(arr, sortType){
-    if(sortType === 'asc'){
-        arr.sort(function(a, b){return a - b});
-    }else if(sortType === 'desc'){
-        arr.sort(function(a, b){return b - a});
-    }
-    return arr;
-}
-solve([14, 7, 17, 6, 8], 'desc');
