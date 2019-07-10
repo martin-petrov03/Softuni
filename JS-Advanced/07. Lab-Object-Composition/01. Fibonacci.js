c012ce331fa1646edbb43a74dcfef44d99e50b9e
martinpetrov000 <martin.petrov033@gmail.com>
2019-07-10 10:15:58 +0300

    Add 01. Fibonacci Problem

---
+++ JS-Advanced/07. Lab-Object-Composition/01. Fibonacci.js
@@ -0,0 +1,15 @@
+function solve(){
+    return (function(){
+        let firstNum = 0;
+        let secondNum = 1;
+
+        return function(){
+            let result = secondNum;
+            let sum = firstNum + secondNum;
+            firstNum = secondNum;
+            secondNum = sum;
+            return result;
+        }
+    })();
+}
+console.log(solve());
