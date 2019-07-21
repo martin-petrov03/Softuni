50ea9a216460eb07d51a45524e3b8d6583122501
martinpetrov000 <martin.petrov033@gmail.com>
2019-07-21 21:27:13 +0300

    Add test js file

---
+++ JS-Advanced/Exam-Preparation-1/02. Softunify/test/test.js
@@ -0,0 +1,36 @@
+let SoftUniFy = require('../02. Softunify');
+const assert = require('chai').assert;
+const expect = require('chai').expect;
+
+describe("test general functionality", function() {
+    let soft;
+    beforeEach(function(){
+        soft = new SoftUniFy();
+    });
+    it("contains allSongs", function() {    
+        expect(soft.allSongs).to.eql({});
+    });    
+});
+describe("test downloadSong functionality", function() {        
+    it("should return array with current values", function() {
+        let song = new SoftUniFy().downloadSong('me', 'songName', 'love');
+        expect(song.allSongs['me']['songs']).to.eql(['songName - love']);        
+    });    
+});
+describe("test playSong functionality", function() {
+    it("contains current song", function() {
+        let current = new SoftUniFy().downloadSong('me', 'songName', 'love');
+        
+        expect(current.playSong('songName')).to.eql('me:\nsongName - love\n');
+    });
+});
+describe("test songsList functionality", function() {
+    it("contains current song", function() {
+        let current = new SoftUniFy();
+        current.downloadSong('me', 'songName', 'love');      
+        expect(current.songsList()).to.be.equal('');      
+    });
+    it("return error message", function() {
+        expect(new SoftUniFy().songsList().length == 0).to.be.equal('Your song list is empty');   
+    });    
+});
