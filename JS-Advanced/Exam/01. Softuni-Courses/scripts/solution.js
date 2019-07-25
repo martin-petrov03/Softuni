f31ed88dd939068605649b5d795a4a693fdd3bee
martinpetrov000 <martin.petrov033@gmail.com>
2019-07-25 17:43:35 +0300

    Add js file

---
+++ JS-Advanced/Exam/01. Softuni-Courses/scripts/solution.js
@@ -0,0 +1,13 @@
+console.log('dsad');
+
+ function solve(){
+     //console.log('dsad');
+     $('#kingdom button').click(function(){
+        let kindomName = document.querySelector('#kingdom div input').value;
+        let kindName = document.querySelector('#kingdom div input:nth-child(2)').value;
+        //console.log('kindom name ');
+        //console.log('king name ' + kindName);
+     });
+     
+ }
+ solve();
