9891e2ef8af723f8461f45f639c428a1dd393ad2
martinpetrov000 <martin.petrov033@gmail.com>
2019-07-25 18:37:28 +0300

    Add js file

--- JS-Advanced/Exam/01. Softuni-Courses/scripts/solution.js
+++ JS-Advanced/Exam/01. Softuni-Courses/scripts/solution.js
@@ -1,13 +1,27 @@
-console.log('dsad');
-
- function solve(){
-     //console.log('dsad');
-     $('#kingdom button').click(function(){
-        let kindomName = document.querySelector('#kingdom div input').value;
-        let kindName = document.querySelector('#kingdom div input:nth-child(2)').value;
-        //console.log('kindom name ');
-        //console.log('king name ' + kindName);
-     });
-     
+ function solve(){    
+ 	$("#availableCourses .courseFoot").click(function(){
+        const courses = [];
+        $.each($("input[type='checkbox']:checked"), function(){  
+            courses.push($(this).val());
+        });        
+        $( "#myCourses .courseBody ul" ).empty();
+        let price = 0;
+        courses.map(course => {
+        	$('#myCourses .courseBody ul').append(`<li>${course}</li>`);
+        	if(course == 'js-fundamentals'){
+        		price += 100;
+        	} else if(course == 'js-advanced'){
+        		price += 200;
+        	} else if(course == 'js-applications'){
+        		price += 300;
+        	} else if(course == 'js-web'){
+        		price += 500;
+        	}
+        });
+        if($('#educationForm input[value="onsite"]').is(":checked")){
+        	price += courses.length * 50;
+        }
+        $('#myCourses .courseFoot p').text(`Cost: ${price.toFixed(2)} BGN`);
+    });
  }
  solve();
