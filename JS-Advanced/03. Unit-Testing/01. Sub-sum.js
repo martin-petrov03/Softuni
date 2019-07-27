d4d105867a91eb961bf09bdf64ba48f33536d46b
Martin Miroslavov Petrov <martin.petrov033@gmail.com>
2019-07-27 15:03:09 +0300

    Update 01. Sub-sum.js

--- JS-Advanced/03. Unit-Testing/01. Sub-sum.js
+++ JS-Advanced/03. Unit-Testing/01. Sub-sum.js
@@ -1,6 +1,3 @@
-//const assert = require('chai').assert;
-//const subSum = require('./01.SubSum');
-
 function subSum(arr, startIndex, endIndex){
     if(!Array.isArray(arr)){
         return NaN;
@@ -20,16 +17,3 @@ function subSum(arr, startIndex, endIndex){
     .reduce((a, b) => a + b);
     return sum;
 }
-// console.log(solve(
-//     'text', 0, 2
-//     ));
-/*describe('subSum', function(){
-    it('first argument must be array', function() {
-        //Arrange
-        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-        //Act
-        let result = subSum(array, 3, 5);
-        //Assert
-        assert.equal(result, 15);
-    });
-});*/
