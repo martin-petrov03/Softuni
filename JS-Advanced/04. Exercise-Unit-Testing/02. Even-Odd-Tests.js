64194ef6c211c4cd0a28ba7f95f3112992facaf9
martinpetrov000 <martin.petrov033@gmail.com>
2019-08-18 16:06:43 +0300

    Add 02. Even Odd Tests

---
+++ JS-Advanced/04. Exercise-Unit-Testing/02. Even-Odd-Tests.js
@@ -0,0 +1,21 @@
+const isOddOrEven = require('../02. Even-Odd');
+const assert = require('chai').assert;
+
+describe('test general functionality and invalid input validation', function(){
+    //Invalid inputs
+    it('should return undefined', function(){
+        assert.equal(isOddOrEven[12], undefined);
+    });
+    it('should return undefined', function(){
+        assert.equal(isOddOrEven([1, {str:'hi'}]), undefined);
+    });
+    it('should return even because the length of input is even', function(){
+        assert.equal(isOddOrEven('haha'), 'even');
+    });
+    it('should return odd because the length of input is odd', function(){
+        assert.equal(isOddOrEven('What\'s up'), 'odd');
+    });
+    it('should return even because the length of input is zero', function(){
+        assert.equal(isOddOrEven(''), 'even');
+    });
+});
