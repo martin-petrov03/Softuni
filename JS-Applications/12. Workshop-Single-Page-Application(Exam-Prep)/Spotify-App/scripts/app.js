46a7e7d7ebebd49db2d5ac8b1c68dc50e7e80ffa
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-07 18:49:09 +0300

    Add app js file

---
+++ JS-Applications/12. Workshop-Single-Page-Application(Exam-Prep)/Spotify-App/scripts/app.js
@@ -0,0 +1,30 @@
+const handlers = {}
+
+$(() => {
+  const app = Sammy('#root', function () {
+    this.use('Handlebars', 'hbs');
+    // home page routes
+    this.get('/index.html', handlers.getHome);
+    this.get('/', handlers.getHome);
+    this.get('#/home', handlers.getHome);
+
+    // user routes
+    this.get('#/register', handlers.getRegister);
+    this.get('#/login', handlers.getLogin);
+
+    this.post('#/register', handlers.registerUser);
+    this.post('#/login', handlers.loginUser);
+    this.get('#/logout', handlers.logoutUser);
+    this.get('#/remove/:id', handlers.removeSong);
+
+    // ADD YOUR ROUTES HERE
+    this.get('#/allSongs', handlers.getAllSongs);
+    this.get('#/createSong', handlers.getCreateSong);
+    this.get('#/mySongs', handlers.getMySongs);
+    this.get('#/like/:id', handlers.likeSong);
+    this.get('#/listen/:id', handlers.listenSong);
+
+    this.post('#/createSong', handlers.createSong);
+  });
+  app.run('#/home');
+});
