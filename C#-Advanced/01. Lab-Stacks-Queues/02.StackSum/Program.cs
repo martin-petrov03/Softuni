c8746d0082405e84aa440ee25de18019c450de45
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-24 19:01:46 +0300

    Add 02.StackSum Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/02.StackSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>(line);
+            string commandInfo = Console.ReadLine().ToLower();
+            
+            while (commandInfo != "end")
+            {
+                string[] items = commandInfo.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (items[0].ToLower())
+                {
+                    case "add":
+                        for (int i = 1; i < items.Length; i++)
+                        {                            
+                            stack.Push(int.Parse(items[i]));
+                        }
+                        break;
+                    case "remove":
+                        int count = int.Parse(items[1]);
+                        if (count <= stack.Count())
+                        {
+                            for (int i = 0; i < count; i++)
+                            {
+                                stack.Pop();
+                            }
+                        }                        
+                        break;
+                    default:
+                        //Console.WriteLine("Invalid Command!");
+                        break;
+                }
+
+                commandInfo = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }        
+    }
+}
