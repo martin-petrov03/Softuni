f8dbb720b046e94f1f3be37790bff27f231e5948
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-24 19:02:29 +0300

    Add 03.SimpleCalculator Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/03.SimpleCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StackSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string[] line = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>(line.Reverse());
+
+            while (stack.Count > 1)
+            {
+                int opr1 = int.Parse(stack.Pop());
+                string opr = stack.Pop();
+                int opr2 = int.Parse(stack.Pop());
+
+                switch (opr)
+                {
+                    case "+":
+                        stack.Push($"{opr1 + opr2}");                  
+                        break;
+                    case "-":
+                        stack.Push($"{opr1 - opr2}");
+                        break;
+                    default:
+                        //Console.WriteLine("Invalid Command!");
+                        break;
+                }
+            }
+                      
+            
+            foreach(string item in stack) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
