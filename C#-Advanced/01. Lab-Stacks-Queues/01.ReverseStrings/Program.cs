366302a8b9350f75df5721274c1b8cc5d6613db1
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-24 18:53:09 +0300

    Add 01.ReverseStrings Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/01.ReverseStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reverse_string
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in input) {
+                stack.Push(ch);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
