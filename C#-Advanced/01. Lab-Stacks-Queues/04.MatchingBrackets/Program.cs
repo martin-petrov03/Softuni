1241b30a91a1b221fde76b7e9f1b368847a4a89b
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-25 19:39:00 +0300

    Add 04.MatchingBrackets Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/04.MatchingBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < line.Length; i++) {
+                if (line[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (line[i] == ')') {
+                    int index = stack.Pop();
+                    Console.WriteLine(line.Substring(index, i - index + 1));
+                }
+            }
+        }
+    }
+}
