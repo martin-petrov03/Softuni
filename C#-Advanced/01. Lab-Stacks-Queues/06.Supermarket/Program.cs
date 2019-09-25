6015ad0919999cb2d5a295bf0b5df913e2086c40
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-25 19:40:53 +0300

    Add 06.Supermarket Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/06.Supermarket/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            string command = Console.ReadLine();
+            int remainingPeople = 0;
+            int totalPeople = 0;
+            bool isPaid = false;
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    isPaid = true;
+                    while (queue.Count > 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                    queue.Clear();
+                    remainingPeople = 0;
+                }
+                else
+                {                
+                    queue.Enqueue(command); 
+                    remainingPeople++;                   
+                    totalPeople++;
+                }
+                command = Console.ReadLine();
+            }
+            if (!isPaid) {
+                remainingPeople = totalPeople;                
+            }
+            
+            Console.WriteLine($"{remainingPeople} people remaining.");
+        }
+    }
+}
