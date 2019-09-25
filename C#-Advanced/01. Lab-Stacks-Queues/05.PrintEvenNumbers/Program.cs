7085f3ab4b7132124a606febd12832e10a1acaee
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-25 19:40:10 +0300

    Add 05.PrintEvenNumbers Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(item => item % 2 == 0)
+                .ToArray();         
+            Console.WriteLine(String.Join(", ", inputArr));
+        }
+    }
+}
