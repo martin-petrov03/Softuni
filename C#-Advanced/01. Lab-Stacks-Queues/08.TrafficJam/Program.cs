ece87f82e50cbbe8f9f42f38ad457f43c82cd438
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-26 19:48:45 +0300

    Add 08.TrafficJam Problem

---
+++ C#-Advanced/01. Lab-Stacks-Queues/08.TrafficJam/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            int numberCarsAtGreen = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int count = 0;
+            while (command.ToLower() != "end") {
+                if (command.ToLower() == "green")
+                {
+                    int currentCount = queue.Count > numberCarsAtGreen ? numberCarsAtGreen : queue.Count;
+                    for (int i = 0; i < currentCount; i++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else {                
+                    queue.Enqueue(command);                    
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
