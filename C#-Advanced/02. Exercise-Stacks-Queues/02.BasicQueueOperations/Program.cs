de7c6ba1fb44d5cb7e429055982dfb42f66ab50f
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-29 19:53:27 +0300

    Add 02.BasicQueueOperations Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] configArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] inputArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> queue = new Queue<int>();
+
+            int countElementsToEnqueue = configArr[0];
+            int countElementsToDequeue = configArr[1];
+            int specialElement = configArr[2];
+
+            for (int i = 0; i < countElementsToEnqueue; i++)
+            {
+                queue.Enqueue(inputArr[i]);
+            }
+            for (int i = 0; i < countElementsToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(specialElement))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine(queue.Min());
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
