aea0a14c723bf68bca11d200e5717ab3ae8cb8eb
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-29 19:52:07 +0300

    Add 01.BasicStackOperations

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/01.BasicStackOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] configArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] inputArr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stack = new Stack<int>();
+
+            int countElementsToPush = configArr[0];
+            int countElementsToPop = configArr[1];
+            int specialElement = configArr[2];
+
+            for (int i = 0; i < countElementsToPush; i++)
+            {               
+               stack.Push(inputArr[i]);                               
+            }
+            for (int i = 0; i < countElementsToPop; i++) {
+                stack.Pop();            
+            }
+
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(specialElement))
+                {
+                    Console.WriteLine("true");
+                }
+                else {
+                    Console.WriteLine(stack.Min());
+                }                
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
