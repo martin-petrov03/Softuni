216af692e0ba0a674629924732ab6193a04d14f1
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-30 19:44:04 +0300

    Add 05.FashionBoutique Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/05.FashionBoutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine().Split(). Select(int.Parse).ToArray();
+            int capacity = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>(line);
+
+            int sum = 0;
+            int countRacks = 1;
+            while (stack.Count > 0)
+            {
+                sum += stack.Peek();
+                
+                //check is it full
+                if (sum <= capacity)
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    countRacks++;
+                    sum = 0;
+                }
+            }
+            Console.WriteLine(countRacks);
+        }
+    }
+}
