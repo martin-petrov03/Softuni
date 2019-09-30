ec06de3e000cc165ebc4c5b0a0377f6a9b52fe34
martinpetrov000 <martin.petrov033@gmail.com>
2019-09-30 19:43:43 +0300

    Add 04.FastFood Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/04.FastFood/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04.FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> leftOrder = new Queue<int>(line);
+
+            Console.WriteLine(line.Max());
+            int i = 0;
+            while (leftOrder.Count > 0) {
+                
+                if (foodQuantity - line[i] >= 0)
+                {
+                   leftOrder.Dequeue();
+                   foodQuantity -= line[i];
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+            if (leftOrder.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", leftOrder)}");
+            }
+            else {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
