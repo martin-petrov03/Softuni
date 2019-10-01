59befb177194b6609eeeab7aff6d0509a918e99b
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-01 18:18:36 +0300

    Add 09.SimpleTextEditor Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            Stack <string> stackOfText = new Stack<string>();
+            string text = String.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (line[0] == "1")
+                {
+                    stackOfText.Push(text);
+                    text += line[1];
+
+                }
+                else if (line[0] == "2")
+                {
+                    int index = int.Parse(line[1]);
+                    stackOfText.Push(text);
+                    text = text.Substring(0, text.Length - index);
+                }
+                else if (line[0] == "3")
+                {
+                    int index = int.Parse(line[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (line[0] == "4")
+                {
+                    text = stackOfText.Pop();
+                }
+            }
+        }
+    }
+}
