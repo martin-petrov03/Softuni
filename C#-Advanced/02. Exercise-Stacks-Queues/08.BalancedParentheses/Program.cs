cf906e955f0386798a1aee546abf0cb563a2ecb4
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-01 18:18:09 +0300

    Add 08.BalancedParentheses Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/08.BalancedParentheses/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08.BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] line = Console.ReadLine()
+                .ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            char[] openParenetheses = new char[] { '(', '[', '{' };
+
+            bool isValid = true;            
+            foreach(char item in line)
+            {            
+                if (openParenetheses.Contains(item)) {
+                    stack.Push(item);
+                    continue;
+                }
+
+                if(stack.Count == 0)
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if (stack.Peek() == '(' && item == ')')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '[' && item == ']')
+                {
+                    stack.Pop();
+                }
+                else if (stack.Peek() == '{' && item == '}')
+                {
+                    stack.Pop();
+                }
+                else {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if (isValid)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
