a2204be5f72a91c4102e02b2c56daea1475e2c0e
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-01 18:17:43 +0300

    Add 07.TruckTour Problem

---
+++ C#-Advanced/02. Exercise-Stacks-Queues/07.TruckTour/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());            
+            Queue<int[]> petrolPumps = new Queue<int[]>();
+
+            int fuel = 0;
+            for (int i = 0; i < n; i++) {
+                int[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                petrolPumps.Enqueue(line);                
+            }
+
+            int index = 0;
+            while (true) {
+                int totalFuel = 0;
+
+                foreach (int[] petrolPump in petrolPumps)
+                {
+                    int amountPetrol = petrolPump[0];
+                    int distanceToNext = petrolPump[1];                    
+
+                    totalFuel += amountPetrol - distanceToNext;
+
+                    if (totalFuel < 0) {
+                        petrolPumps.Enqueue(petrolPumps.Dequeue());
+                        index++;
+                        break;
+                    }
+                }
+                if (totalFuel >= 0) {
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+        }
+    }
+}
