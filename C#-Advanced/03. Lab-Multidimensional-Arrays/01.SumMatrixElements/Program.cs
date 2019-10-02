b23a00363c5d1e2beeba8779dca0b8c5de72c330
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-02 18:31:24 +0300

    Add 01.SumMatrixElements Problem

---
+++ C#-Advanced/03. Lab-Multidimensional-Arrays/01.SumMatrixElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int countRows = count[0];
+            int countColumns = count[1];
+
+            int[,] matrix = new int[countRows, countColumns];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                }
+            }
+
+            Console.WriteLine(countRows);
+            Console.WriteLine(countColumns);
+
+            int sum = 0;
+            foreach (int item in matrix)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
