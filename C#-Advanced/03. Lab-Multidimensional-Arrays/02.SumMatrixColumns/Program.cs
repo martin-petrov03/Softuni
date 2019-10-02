90c2f5ed3a95659139ec5c97f874fbd5590c59db
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-02 18:31:48 +0300

    Add 02.SumMatrixColumns Problem

---
+++ C#-Advanced/03. Lab-Multidimensional-Arrays/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] count = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int countRows = count[0];
+            int countColumns = count[1];
+
+            int[,] matrix = new int[countRows, countColumns];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = tokens[j];
+                }
+            }
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                int sum = 0;
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    sum += matrix[row, col];
+                }
+                Console.WriteLine(sum);
+            }
+
+        }
+    }
+}
