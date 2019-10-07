e851e7b8e1bdd0a7d6a06884a3eb20fc4c3f5b2e
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-07 20:22:52 +0300

    Add 04.MatrixShuffling Problem

---
+++ C#-Advanced/04. Exercise-Multidimensional-Arrays/04.MatrixShuffling/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToArray();
+            int countRows = matrixSize[0];
+            int countCols = matrixSize[1];
+            string[,] matrix = new string[countRows, countCols];
+
+            //fill the matrix
+            for (int row = 0; row < countRows; row++)
+            {
+                string[] line = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < countCols; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+            
+            string[] command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);            
+
+            while (command[0] != "END")
+            {
+                if (command[0] == "swap")
+                {                    
+                    try
+                    {                   
+                        string firstValueTemp = matrix[int.Parse(command[1]), int.Parse(command[2])];
+                        string secondValueTemp = matrix[int.Parse(command[3]), int.Parse(command[4])];
+                        
+                        matrix[int.Parse(command[3]), int.Parse(command[4])] = firstValueTemp;
+                        matrix[int.Parse(command[1]), int.Parse(command[2])] = secondValueTemp;
+
+                        for (int row = 0; row < countRows; row++)
+                        {                             
+                            for (int col = 0; col < countCols; col++)
+                            {
+                                Console.Write(matrix[row, col] + " ");
+                            }
+                            Console.WriteLine();
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid input!");
+                    }                                     
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+            command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
