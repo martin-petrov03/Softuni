5506c54b55cbd2dbc8fd999e76e4d0d496f54f37
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-09 18:41:00 +0300

    Add 02.AverageStudentGrades Problem

---
+++ C#-Advanced/05. Lab-Sets-Dictionaries-Advanced/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countLines = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < countLines; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (students.ContainsKey(line[0]))
+                {
+                    students[line[0]].Add(double.Parse(line[1]));
+                }
+                else
+                {
+                    students[line[0]] = new List<double>();
+                    students[line[0]].Add(double.Parse(line[1]));
+                   
+                }
+            }
+
+            foreach (var student in students)
+            {
+                string name = student.Key;
+                var grades = student.Value;
+                double average = grades.Average();
+
+                Console.Write($"{name} -> ");
+                foreach (double grade in grades)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                Console.Write($"(avg: {average:f2})");
+                Console.WriteLine();
+            }
+        }
+    }
+}
