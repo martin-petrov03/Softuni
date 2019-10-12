2988ea8606aae856b012ce178c83cf3b239c46b4
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-12 16:09:23 +0300

    Add 06.ParkingLot Problem

---
+++ C#-Advanced/05. Lab-Sets-Dictionaries-Advanced/06.ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string[] line = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (line[0] != "END")
+            {
+                if (line[0] == "IN")
+                {
+                    cars.Add(line[1]);
+                }
+                else if (line[0] == "OUT")
+                {
+                    cars.Remove(line[1]);
+                }
+
+                line = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (string car in cars)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
