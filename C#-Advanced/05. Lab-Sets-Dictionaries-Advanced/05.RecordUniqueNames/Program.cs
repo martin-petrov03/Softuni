7f31e88b547876e9ef86451000e57b08d0184e3e
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-12 16:06:48 +0300

    Add 05.RecordUniqueNames Problem

---
+++ C#-Advanced/05. Lab-Sets-Dictionaries-Advanced/05.RecordUniqueNames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countLines = int.Parse(Console.ReadLine());
+
+            HashSet<string> uniqueNames = new HashSet<string>();
+            for (int i = 0; i < countLines; i++)
+            {
+                string name = Console.ReadLine();
+                uniqueNames.Add(name);
+            }
+            
+            foreach (string name in uniqueNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
