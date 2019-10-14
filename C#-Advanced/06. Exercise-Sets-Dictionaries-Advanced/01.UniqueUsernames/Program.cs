5e61c98d644f4e843a2faa6831090d5a7603ef2d
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-14 17:39:13 +0300

    Add 01.UniqueUsernames Problem

---
+++ C#-Advanced/06. Exercise-Sets-Dictionaries-Advanced/01.UniqueUsernames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+            int countLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countLines; i++)
+            {
+                string input = Console.ReadLine();
+                uniqueUsernames.Add(input);
+            }
+
+            foreach (string username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
