5a410f22d773eb6a9cc8754fa3a13ad01531d584
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-14 17:37:50 +0300

    Add 07.SoftUni-Party Problem

---
+++ C#-Advanced/05. Lab-Sets-Dictionaries-Advanced/07.SoftUni-Party/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vips = new HashSet<string>();
+            HashSet<string> otherPeople = new HashSet<string>();
+
+            string line = Console.ReadLine();            
+            while (line != "PARTY")
+            {
+                if (Char.IsDigit(line[0]))
+                {
+                    vips.Add(line);
+                }
+                else
+                {
+                    otherPeople.Add(line);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            line = Console.ReadLine();            
+            while (line != "END")
+            {
+                if (Char.IsDigit(line[0]))
+                {
+                    vips.Remove(line);
+                }
+                else
+                {
+                    otherPeople.Remove(line);
+                }
+
+                line = Console.ReadLine();
+            }
+           
+            Console.WriteLine(vips.Count + otherPeople.Count);
+            foreach (string currentVip in vips)
+            {
+                Console.WriteLine(currentVip);
+            }
+            foreach (string person in otherPeople)
+            {                
+                Console.WriteLine(person);
+            }           
+        }
+    }
+}
