7053218d2940d69c3ae94e296e3d72864f916faf
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-15 16:55:42 +0300

    Add 03.PeriodicTable Problem

---
+++ C#-Advanced/06. Exercise-Sets-Dictionaries-Advanced/03.PeriodicTable/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int countLines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countLines; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (string item in input)
+                {
+                    elements.Add(item);
+                }
+            }
+            
+            foreach (string element in elements)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
