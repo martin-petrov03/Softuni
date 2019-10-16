41c69c1eb787c0ce09a27e3d98174ebb3bf4fa35
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-16 19:07:41 +0300

    Add 05.CountSymbols Problem

---
+++ C#-Advanced/06. Exercise-Sets-Dictionaries-Advanced/05.CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> symbolsCount = new SortedDictionary<char, int>();
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (!symbolsCount.ContainsKey(currentChar))
+                {
+                    symbolsCount.Add(currentChar, 0);
+                }
+                symbolsCount[currentChar]++;
+            }
+            
+            foreach (var (key, value) in symbolsCount)
+            {
+                Console.WriteLine($"{key}: {value} time/s");
+            }
+        }
+    }
+}
