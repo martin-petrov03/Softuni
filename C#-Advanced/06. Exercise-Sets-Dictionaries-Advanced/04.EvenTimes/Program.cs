be88f7fd78b95c4da7f459ae4bf361556734337b
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-16 19:07:18 +0300

    Add 04.EvenTimes Problem

---
+++ C#-Advanced/06. Exercise-Sets-Dictionaries-Advanced/04.EvenTimes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> repetCount = new Dictionary<int, int>();
+
+            int countLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countLines; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (!repetCount.ContainsKey(currentNum))
+                {
+                    repetCount.Add(currentNum, 0);
+                }               
+                repetCount[currentNum]++;             
+            }
+
+            int evenTimesNum = repetCount                
+                .SingleOrDefault(num => num.Value % 2 == 0)
+                .Key;
+
+            Console.WriteLine(evenTimesNum);
+        }
+    }
+}
