97775e08b8a2edbf4cdecff88b202e96a079deae
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-21 21:22:25 +0300

    Add 02.LineNumbers Problem

---
+++ C#-Advanced/07. Lab-Streams-Files-Directories/02.LineNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string inputFilePath = @"../../../text.txt";
+            string outputFilePath = @"../../../output.txt";
+            string[] textLines = File.ReadAllLines(inputFilePath);
+
+            int lineCounter = 1;
+            foreach (string currentLine in textLines)
+            {
+                int letterCount = currentLine.Count(char.IsLetter);
+                int punctuationsCount = currentLine.Count(char.IsPunctuation);
+
+                string text = $"Line {lineCounter}: {currentLine} ({letterCount})({punctuationsCount})";
+
+                if (lineCounter != textLines.Length)
+                {
+                    text += Environment.NewLine;
+                }
+
+                File.AppendAllText(outputFilePath, text);
+                lineCounter++;
+            }
+        }
+    }
+}
