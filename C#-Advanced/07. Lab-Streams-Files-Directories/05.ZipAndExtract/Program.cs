11cc2d11b1985c8add4c056cb2de348dc95b9ef6
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-22 17:09:15 +0300

    Add 05.ZipAndExtract Problem

---
+++ C#-Advanced/07. Lab-Streams-Files-Directories/05.ZipAndExtract/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _05.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pictureFilesPath = "copyMe.png";
+            string textFilesPath = "text.txt";
+            string targetPath = "../../../result.zip";
+            
+            using (var archive = ZipFile.Open(targetPath, ZipArchiveMode.Create))
+            {
+                archive.CreateEntryFromFile(pictureFilesPath, Path.GetFileName(pictureFilesPath));
+                archive.CreateEntryFromFile(textFilesPath, Path.GetFileName(textFilesPath));
+            }
+        }
+    }
+}
