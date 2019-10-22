17a736614d8a20ccb324be02819eb99c2bee70f7
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-22 17:08:39 +0300

    Add 04.CopyBinaryFile Problem

---
+++ C#-Advanced/07. Lab-Streams-Files-Directories/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pictureFilePath = @"../../../copyMe.png";
+            string copiedPictureFilePath = @"../../../copyMe-copyMe-copy.png";
+
+            using (FileStream streamReader = new FileStream(pictureFilePath, FileMode.Open))
+            {
+                using (FileStream streamWriter = new FileStream(copiedPictureFilePath, FileMode.Create))
+                {
+                    while (true)
+                    {
+                        byte[] byteArr = new byte[4096];
+                        int size = streamReader.Read(byteArr, 0, byteArr.Length);
+                        if (size == 0)
+                        {
+                            break;
+                        }
+                        streamWriter.Write(byteArr, 0, size);                        
+                    }
+                }
+            }
+        }
+    }
+}
