89c8c746387cbb52d00f7e9c6967a8f0c5d764f6
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-23 19:53:47 +0300

    Add 02.SumNumbers Problem

---
+++ C#-Advanced/08. Lab-Functional-Programming/02.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(Parse)
+                .ToArray();
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+
+        }
+        public static Func<string, int> Parse = n => Int32.Parse(n);
+    }
+}
