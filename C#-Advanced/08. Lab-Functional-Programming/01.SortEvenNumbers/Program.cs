5649d226612425041b1731fd1c536628989120d5
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-23 19:53:24 +0300

    Add 01.SortEvenNumbers Problem

---
+++ C#-Advanced/08. Lab-Functional-Programming/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(", ", Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)));
+        }
+    }
+}
