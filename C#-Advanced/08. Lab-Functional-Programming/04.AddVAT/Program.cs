6c7bdac27d0b73110c2b2eeaf31327ae672a5343
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-24 20:00:25 +0300

    Add 04.AddVAT Problem

---
+++ C#-Advanced/08. Lab-Functional-Programming/04.AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList()                
+                .ForEach(x => Console.WriteLine($"{(x + 0.2 * x):f2}"));
+        }
+    }
+}
