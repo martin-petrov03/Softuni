862d256cdc8f9effe7cba6e232cb0e32b92bc4e3
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-26 09:42:23 +0300

    Add 01.ActionPoint Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/01.ActionPoint/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printNames = names =>
+            Console.WriteLine(String.Join(Environment.NewLine, names));
+
+            string[] inputNames = Console.ReadLine()
+                .Split();
+            printNames(inputNames);            
+        }
+    }
+}
