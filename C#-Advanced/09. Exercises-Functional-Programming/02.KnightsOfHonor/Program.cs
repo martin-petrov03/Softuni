e024d2f566e53ac00be96329ed4448d7f5f69b83
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-27 19:39:13 +0200

    Add 02.KnightsOfHonor Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> printFormatedNames = names =>
+            Console.WriteLine("Sir " + String.Join(Environment.NewLine + "Sir ", names));
+
+            string[] inputNames = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            printFormatedNames(inputNames);
+        }
+    }
+}
