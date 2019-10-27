80522cabcb19b257f59ce78bf30852c019faed86
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-27 19:39:35 +0200

    Add 03.CustomMinFunction Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Func<int[], int> minValue = arrOfNums =>
+            {
+                int min = int.MaxValue;
+                foreach (int currentNum in arrOfNums)
+                {
+                    if (min > currentNum)
+                    {
+                        min = currentNum;
+                    }
+                }
+                return min;
+            };
+
+            int[] inputNums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(minValue(inputNums));
+        }
+    }
+}
