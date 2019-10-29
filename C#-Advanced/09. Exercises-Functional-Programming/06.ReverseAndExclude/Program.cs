c3179c635e04e77df234a70ea2ea26773e8804bd
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-29 19:25:56 +0200

    Add 06.ReverseAndExclude Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int devider = int.Parse(Console.ReadLine());
+       
+            numbers = numbers
+                .Where(x => x % devider != 0)
+                .Reverse()
+                .ToArray();
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
