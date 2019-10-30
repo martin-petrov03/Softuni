bf8e26598abfed7b6d86278e32a4f40ee0765118
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-30 19:34:03 +0200

    Add 07. PredicateForNames Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/07. PredicateForNames/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split();
+            List<string> result = new List<string>();
+
+            foreach (string name in names)
+            {
+                if (name.Length <= length)
+                {
+                    result.Add(name);
+                }                
+            }
+
+            Console.WriteLine(String.Join("\n", result));
+        }
+    }
+}
