794aad902d13bf6b6e9d634af79acc85b031e5e7
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-31 17:37:27 +0200

    Add 09.ListОfPredicates Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/09.ListОfPredicates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            List<int> nums = Enumerable.Range(1, num).ToList();
+            int[] deviders = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Distinct()
+                .ToArray();
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            foreach (int currentNumber in deviders)
+            {
+                predicates.Add(x => x % currentNumber == 0);
+            }
+            for (int i = 0; i < nums.Count; i++)
+            {                
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(nums[i]))
+                    {
+                        nums.Remove(nums[i]);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
