cdfb3b2224b364bab415b597155893f33f452875
martinpetrov000 <martin.petrov033@gmail.com>
2019-10-31 17:36:39 +0200

    Add 08.CustomComparator Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/08.CustomComparator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputArr = new List<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+            List<int> evenNums = inputArr.Where(x => x % 2 == 0).ToList();
+            List<int> oddNums = inputArr.Where(x => x % 2 != 0).ToList();
+
+            evenNums.Sort();
+            oddNums.Sort();
+
+            Console.WriteLine($"{String.Join(" ", evenNums)} {String.Join(" ", oddNums)}");
+        }
+    }
+}
