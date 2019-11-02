75f3435dbb14a71873efd38a64ba32121222bd68
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-02 16:14:09 +0200

    Add 11.TriFunction Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/11.TriFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _11.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int specialNum = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isLarger = (name, num)
+                => name.Sum(x => x) >= num;
+            Func<string[], Func<string, int, bool>, string> nameFilter = (inputName, isLargerFilter)
+                => inputName.FirstOrDefault(x => isLargerFilter(x, specialNum));
+
+            string resultName = nameFilter(input, isLarger);
+            Console.WriteLine(resultName);
+        }
+    }
+}
