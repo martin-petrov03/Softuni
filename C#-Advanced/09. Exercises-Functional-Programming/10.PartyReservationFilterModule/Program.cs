ed862782af311bf31948a1a1700f2a81d4065e4f
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-02 16:13:36 +0200

    Add 10.PartyReservationFilterModule Problem

---
+++ C#-Advanced/09. Exercises-Functional-Programming/10.PartyReservationFilterModule/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.PartyReservationFilterModule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string filter = Console.ReadLine();
+
+            List<string> filters = new List<string>();
+
+            while (filter != "Print")
+            {
+                string[] filterInfo = filter.Split(";");
+                if (filterInfo[0] == "Add filter")
+                {
+                    filters.Add($"{filterInfo[1]};{filterInfo[2]}");
+                }
+                else if (filterInfo[0] == "Remove filter")
+                {
+                    filters.Remove($"{filterInfo[1]};{filterInfo[2]}");
+                }
+
+                filter = Console.ReadLine();
+            }
+
+            Func<string, int, bool> lengthFilter = (name, length) => name.Length == length;
+            Func<string, string, bool> startsWithFilter = (name, param) => name.StartsWith(param);
+            Func<string, string, bool> endsWithFilter = (name, param) => name.EndsWith(param);
+            Func<string, string, bool> containsFilter = (name, param) => name.Contains(param);
+
+            foreach (var currentFilter in filters)
+            {
+                string[] currentFilterInfo = currentFilter.Split(";");
+                string action = currentFilterInfo[0];
+                string param = currentFilterInfo[1];
+                if (action == "Starts with")
+                {
+                    names = names.Where(n => !startsWithFilter(n, param)).ToArray();
+                }
+                else if (action == "Ends with")
+                {
+                    names = names.Where(n => !endsWithFilter(n, param)).ToArray();
+                }
+                else if (action == "Length")
+                {
+                    names = names.Where(n => !lengthFilter(n, int.Parse(param))).ToArray();
+                }
+                else if (action == "Contains")
+                {
+                    names = names.Where(n => !containsFilter(n, param)).ToArray();
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", names));
+        }
+    }
+}
