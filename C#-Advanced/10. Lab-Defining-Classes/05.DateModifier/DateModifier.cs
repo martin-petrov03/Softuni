fe85703f25c16a7ca84b82ef517ba4eb0299dbf9
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-05 15:43:53 +0200

    Add 05.DateModifier Problem

---
+++ C#-Advanced/10. Lab-Defining-Classes/05.DateModifier/DateModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class DateModifier
+    {
+        private DateTime firstDate;
+        private DateTime secondDate;
+        private int difference;
+
+        public int Dates()
+        {
+            difference = Math.Abs((firstDate.Date - secondDate.Date).Days);
+            return difference;
+        }
+
+        public DateTime FirstDate
+        {
+            set { this.firstDate = value; }
+        }
+
+        public DateTime SecondDate
+        {
+            set { this.secondDate = value; }
+        }
+    }
+}
---
+++ C#-Advanced/10. Lab-Defining-Classes/05.DateModifier/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] frirstInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            DateTime firstDate = new DateTime(frirstInput[0], frirstInput[1], frirstInput[2]);
+            DateTime secondDate = new DateTime(secondInput[0], secondInput[1], secondInput[2]);
+
+            DateModifier differenceBetweenTwoDates = new DateModifier();
+            differenceBetweenTwoDates.FirstDate = firstDate;
+            differenceBetweenTwoDates.SecondDate = secondDate;
+
+            Console.WriteLine(differenceBetweenTwoDates.Dates());
+        }
+    }
+}
