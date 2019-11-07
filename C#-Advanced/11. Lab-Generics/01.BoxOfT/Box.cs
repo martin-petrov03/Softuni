bb4b2d6e968222936f04deef9f0140e4597925d6
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-07 16:19:43 +0200

    Add 01.BoxOfT Problem

---
+++ C#-Advanced/11. Lab-Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> boxItems;
+
+        public int Count { get { return this.boxItems.Count; } }
+
+        public Box()
+        {
+            this.boxItems = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.boxItems.Add(element);
+        }
+
+        public T Remove()
+        {            
+            T lastElement = boxItems.Last();
+            this.boxItems.RemoveAt(boxItems.Count - 1);
+            return lastElement;
+        }
+    }
+}
