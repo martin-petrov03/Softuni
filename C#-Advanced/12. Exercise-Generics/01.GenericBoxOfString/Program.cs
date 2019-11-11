d3e264cff2a05657036de51a54a1a43ead3fc2be
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-11 17:38:04 +0200

    Add 01.GenericBoxOfString Problem

---
+++ C#-Advanced/12. Exercise-Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+
+            int countLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countLines; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            string result = box.ToString();
+            Console.WriteLine(result); ;
+        }
+    }
+}
