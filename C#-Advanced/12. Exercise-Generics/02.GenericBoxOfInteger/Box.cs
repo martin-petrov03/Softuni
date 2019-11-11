c8a936118676e112728cd84391670fa898c7004b
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-11 17:38:23 +0200

    Add 02.GenericBoxOfInteger Problem

---
+++ C#-Advanced/12. Exercise-Generics/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public List<T> box;
+        public Box()
+        {
+            this.box = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            this.box.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (T item in this.box)
+            {
+                sb.AppendLine($"{item.GetType().FullName}: {item}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C#-Advanced/12. Exercise-Generics/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericBoxOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            int countLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countLines; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+
+            string result = box.ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
