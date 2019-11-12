550656f17b8d738e04859a115b7700da7b5f3d29
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-12 16:58:24 +0200

    Add 04.GenericSwapMethodIntegers Problem

---
+++ C#-Advanced/12. Exercise-Generics/04.GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSwapMethodIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            int countLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countLines; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                box.Add(input);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            box.Swap(indexes[0], indexes[1]);
+
+            string result = box.ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
