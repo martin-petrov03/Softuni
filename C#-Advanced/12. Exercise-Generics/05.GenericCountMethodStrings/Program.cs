3d7a3775201d3147c2f819216b94d5c077cf3caf
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-13 20:40:02 +0200

    Add 05.GenericCountMethodStrings Problem

---
+++ C#-Advanced/12. Exercise-Generics/05.GenericCountMethodStrings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCountMethodStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                box.Add(input);
+            }
+
+            string stringToCompare = Console.ReadLine();
+            box.Compare(stringToCompare);
+            int result = box.CountGreater;
+
+            Console.WriteLine(result);
+        }
+    }
+}
