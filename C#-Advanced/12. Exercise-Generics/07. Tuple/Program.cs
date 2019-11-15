f5be118a5423b52bf91f76eca63fac70ac9db48c
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-15 15:43:16 +0200

    Add 07. Tuple Problem

---
+++ C#-Advanced/12. Exercise-Generics/07. Tuple/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split();
+
+            string personName = personInfo[0] + " " + personInfo[1];
+            string personTown = personInfo[2];
+
+            string[] personBeerInfo = Console.ReadLine()
+                .Split();
+
+            string[] numbers = Console.ReadLine()
+                .Split();
+
+            int integer = int.Parse(numbers[0]);
+            double d = double.Parse(numbers[1]);
+
+            
+
+            Tuple<string, string> personTyple = new Tuple<string, string>(personName, personTown);
+            Tuple<string, int> personBeerTyple = new Tuple<string, int>(personBeerInfo[0], int.Parse(personBeerInfo[1]));
+            Tuple<int, double> numbersTyple = new Tuple<int, double>(integer, d);
+
+            Console.WriteLine(personTyple.GetInfo());
+            Console.WriteLine(personBeerTyple.GetInfo());
+            Console.WriteLine(numbersTyple.GetInfo());
+        }
+    }
+}
