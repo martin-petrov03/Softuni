50d507d6ce31a11c287d65213fc374ea697a6f04
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-16 16:30:57 +0200

    Add 02. Collection Problem

---
+++ C#-Advanced/13. Lab-Iterators-Comparators/02. Collection/Collection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    public class Collection<T> : IEnumerable<T>
+    {
+        private List<T> items;
+        private int index;
+
+        public Collection(List<T> items)
+        {
+            this.items = items;
+            this.index = 0;
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.index++;
+            }
+            else {
+                return false;
+            }
+            return true;
+        }
+
+        public bool HasNext()
+        {
+            if (this.index < this.items.Count - 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.items.Count == 0)
+            {
+                throw new Exception("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.items[this.index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var currentItem in this.items)
+            {
+                yield return currentItem;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ C#-Advanced/13. Lab-Iterators-Comparators/02. Collection/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection<string> collection = null;
+
+            string[] line = Console.ReadLine()
+                .Split();
+
+            while (line[0] != "END")
+            {
+                try
+                {
+                    if (line[0] == "Create")
+                    {
+                        List<string> items = line.Skip(1).ToList();
+                        collection = new Collection<string>(items);
+                    }
+                    else if (line[0] == "Print")
+                    {
+                        collection.Print();
+                    }
+                    else if (line[0] == "Move")
+                    {
+                        Console.WriteLine(collection.Move());
+                    }
+                    else if (line[0] == "HasNext")
+                    {
+                        Console.WriteLine(collection.HasNext());
+                    }
+                    else if (line[0] == "PrintAll")
+                    {
+                        foreach (var item in collection)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Operation!");
+                }
+
+                line = Console.ReadLine()
+                    .Split();
+            }
+        }
+    }
+}
