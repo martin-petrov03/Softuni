0cc30276005dc068ca83e3a091640efec92d19dd
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-17 17:03:09 +0200

    Add 04.Froggy Problem

---
+++ C#-Advanced/13. Lab-Iterators-Comparators/04.Froggy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(nums);
+
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
