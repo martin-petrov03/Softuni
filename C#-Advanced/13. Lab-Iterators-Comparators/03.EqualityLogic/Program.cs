1cd78e7bc5ee7c2b0eea3a7d7ac6e50933a19897
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-17 17:02:40 +0200

    Add 03.EqualityLogic Problem

---
+++ C#-Advanced/13. Lab-Iterators-Comparators/03.EqualityLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Person> hashSet = new HashSet<Person>();
+            SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+            int countLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < countLines; i++)
+            {
+                string[] items = Console.ReadLine()
+                    .Split();
+                string name = items[0];
+                int age = int.Parse(items[1]);
+
+                Person currentPerson = new Person(name, age);
+                hashSet.Add(currentPerson);
+                sortedSet.Add(currentPerson);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
