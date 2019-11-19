57819131d618a00fa1a16fc65d8595054dfa390e
martinpetrov000 <martin.petrov033@gmail.com>
2019-11-19 19:00:47 +0200

    Add LaptopShop Project

---
+++ C#-Advanced/Workshops/LaptopShop/LaptopShop/LaptopShop.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LaptopShop
+{
+    public class LaptopShop
+    {
+        private Dictionary<string, List<Laptop>> laptops = new Dictionary<string, List<Laptop>>();
+
+        public LaptopShop()
+        {
+            this.laptops = new Dictionary<string, List<Laptop>>();
+        }
+
+        public int Count
+            => this.laptops.Count;
+
+        public void AddLaptop(Laptop laptop)
+        {
+            IfNullThrowException(laptop);
+
+            if (!this.laptops.ContainsKey(laptop.Make))
+            {
+                this.laptops.Add(laptop.Make, new List<Laptop>());
+            }
+            this.laptops[laptop.Make].Add(laptop);
+        }
+
+        public bool RemoveLaptop(Laptop laptop)
+        {
+            IfNullThrowException(laptop);
+
+            if (!this.laptops.ContainsKey(laptop.Make) || !this.laptops[laptop.Make].Contains(laptop))
+            {
+                return false;
+            }
+
+            bool isRemoved = this.laptops[laptop.Make].Remove(laptop);
+
+            if (this.laptops[laptop.Make].Count == 0)
+            {
+                isRemoved = this.laptops.Remove(laptop.Make);
+            }
+
+            return isRemoved;
+        }
+
+        public bool ContainsLaptop(Laptop laptop)
+        {
+            if (!this.laptops.ContainsKey(laptop.Make) || !this.laptops[laptop.Make].Contains(laptop))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void printAllLaptops(Action<Laptop> action)
+        {
+            foreach (var (make, laptops) in this.laptops)
+            {
+                foreach (var laptop in laptops)
+                {
+                    action(laptop);
+                }
+            }
+        }
+
+        public void IfNullThrowException(Laptop laptop)
+        {
+            if (laptop == null)
+            {
+                throw new ArgumentNullException(nameof(laptop), "Object cannot be null");
+            }
+        }
+    }
+}
---
+++ C#-Advanced/Workshops/LaptopShop/LaptopShop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LaptopShop
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            string laptopMake = "Acer";
+            string laptopModel = "Aspire 5";
+            double laptopDisplaySize = 15.6;
+            decimal laptopPrice = 1500;
+            int laptopRam = 8;
+            int laptopSsd = 480;
+
+            Laptop laptop = new Laptop(laptopMake, laptopModel, laptopDisplaySize, laptopPrice, laptopRam, laptopSsd);
+            Laptop laptop1 = new Laptop("Lenovo", "V130-15IKB", laptopDisplaySize, 2000, 16, 512);
+            LaptopShop shop = new LaptopShop();
+
+            shop.AddLaptop(laptop);
+            shop.AddLaptop(laptop1);
+            shop.printAllLaptops(x => Console.WriteLine(x.FullInfo()));
+        }
+    }
+}
