5a9290e622c772a93ec26d4cbce48f41d65a8b73
martinpetrov000 <martin.petrov033@gmail.com>
2019-12-01 12:41:13 +0200

    Add 03.FightingArena Problem

---
+++ C#-Advanced/Exam-preparation-2/03.FightingArena/FightingArena/Arena.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Arena
+    {
+        private List<Gladiator> gladiators { get; set; }
+        
+        public int Count => this.gladiators.Count();
+
+        public string Name { get; private set; }
+
+        public Arena(string name)
+        {
+            this.Name = name;
+            this.gladiators = new List<Gladiator>();
+        }        
+
+        public void Add(Gladiator gladiator)
+        {
+            gladiators.Add(gladiator);
+        }
+
+        public void Remove(string name)
+        {
+            Gladiator targetGladiator = gladiators.FirstOrDefault(g => g.Name == name);
+            gladiators.Remove(targetGladiator);
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            Gladiator targetGladiator = gladiators.OrderByDescending(g => g.GetStatPower()).First();
+            return targetGladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            Gladiator targetGladiator = gladiators.OrderByDescending(g => g.GetWeaponPower()).First();
+            return targetGladiator;
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            Gladiator targetGladiator = gladiators.OrderByDescending(g => g.GetTotalPower()).First();
+            return targetGladiator;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"[{this.Name}] - [{this.Count}] gladiators are participating.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#-Advanced/Exam-preparation-2/03.FightingArena/FightingArena/Gladiator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FightingArena
+{
+    public class Gladiator
+    {
+        public string Name { get; set; }
+
+        public Stat Stat { get; set; }
+
+        public Weapon Weapon { get; set; }
+
+        public Gladiator(string name, Stat stat, Weapon weapon)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Weapon = weapon;
+        }
+
+        public int GetTotalPower()
+        {
+            return this.GetStatPower() + this.GetWeaponPower();
+        }
+
+        public int GetWeaponPower()
+        {
+            return this.Weapon.Size + this.Weapon.Solidity + this.Weapon.Sharpness;
+        }
+
+        public int GetStatPower()
+        {
+            return this.Stat.Intelligence + this.Stat.Skills + this.Stat.Strength + this.Stat.Agility + this.Stat.Flexibility;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"[{this.Name}] - [{this.GetTotalPower()}]");
+            sb.AppendLine($"  Weapon Power: [{this.GetWeaponPower()}]");
+            sb.AppendLine($"  Stat Power: [{this.GetStatPower()}]");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
