2335a65caa3dc3df9019b0143784e32f70141d7d
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-12 21:31:46 +0200

    Add 02.SpaceStationRecruitment Problem

---
+++ C#-Advanced/Exam/02.SpaceStationRecruitment/SpaceStation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStationRecruitment
+{
+    public class SpaceStation
+    {
+        private List<Astronaut> data { get; set; }
+
+        public int Count { get { return this.data.Count; } }        
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public SpaceStation(string name, int capacity)
+        {
+            this.Name = name;            
+            this.Capacity = capacity;
+            this.data = new List<Astronaut>();
+        }
+
+        public void Add(Astronaut astronaut)
+        {
+            if (!this.data.Contains(astronaut))
+            {
+                if (this.Capacity >= 0)
+                {
+                    this.data.Add(astronaut);
+                    this.Capacity--;
+                }
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Astronaut targetAstronaut = this.data.FirstOrDefault(x => x.Name == name);
+            if (targetAstronaut != null)
+            {
+                this.data.Remove(targetAstronaut);
+                this.Capacity++;
+                return true;
+            }
+            return false;
+        }
+
+        public Astronaut GetOldestAstronaut()
+        {
+            Astronaut targetAstronaut = this.data.OrderByDescending(x => x.Age).First();
+            return targetAstronaut;
+        }
+
+        public Astronaut GetAstronaut(string name)
+        {
+            Astronaut targetAstronaut = this.data.FirstOrDefault(x => x.Name == name);            
+            return targetAstronaut;            
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Astronauts working at Space Station {this.Name}:");
+            foreach (Astronaut item in this.data)
+            {
+                sb.AppendLine(item.ToString());        
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#-Advanced/Exam/02.SpaceStationRecruitment/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SpaceStationRecruitment
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            SpaceStation spaceStation = new SpaceStation("Apolo", 2);
+            
+            Astronaut astronaut = new Astronaut("Stephen", 40, "Bulgaria");
+            
+            Console.WriteLine(astronaut); //Astronaut: Stephen, 40 (Bulgaria)
+
+            
+            spaceStation.Add(astronaut);
+            
+            spaceStation.Remove("Astronaut name");
+
+            Astronaut secondAstronaut = new Astronaut("Mark", 34, "UK");
+
+            
+            spaceStation.Add(secondAstronaut);
+
+            Astronaut oldestAstronaut = spaceStation.GetOldestAstronaut(); // Astronaut with name Stephen
+            Astronaut astronautStephen = spaceStation.GetAstronaut("Stephen"); // Astronaut with name Stephen
+            Console.WriteLine(oldestAstronaut); //Astronaut: Stephen, 40 (Bulgaria)
+            Console.WriteLine(astronautStephen); //Astronaut: Stephen, 40 (Bulgaria)
+
+            Console.WriteLine(spaceStation.Count); //2
+
+            Console.WriteLine(spaceStation.Report());
+            //Astronauts working at Space Station Apolo:
+            //Astronaut: Stephen, 40 (Bulgaria)
+            //Astronaut: Mark, 34 (UK)
+
+        }
+    }
+}
