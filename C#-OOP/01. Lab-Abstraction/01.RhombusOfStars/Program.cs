566431596ef72f74794354802744ba2ea76fd9c7
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-17 20:12:18 +0200

    Add 01.RhombusOfStars Problem

---
+++ C#-OOP/01. Lab-Abstraction/01.RhombusOfStars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace RhombusOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            //Top part
+            for (int i = 0; i <= n; i++)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+
+            //Bottom part
+            for (int i = n - 1; i > 0; i--)
+            {
+                sb.Append(new string(' ', n - i));
+                DrawLineOfStars(sb, i);
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        public static void DrawLineOfStars(StringBuilder sb, int numberOfStars) {
+            for (int star = 0; star < numberOfStars; star++)
+            {
+                sb.Append('*');
+                if (star < numberOfStars - 1)
+                {
+                    sb.Append(' ');
+                }
+            }
+            sb.AppendLine();
+        }
+    }
+}
