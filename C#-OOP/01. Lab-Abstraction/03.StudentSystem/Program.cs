bf19303b4902db4ba54d8c367b32536de46c53b5
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-20 15:30:53 +0200

    Add 03.StudentSystem Problem

---
+++ C#-OOP/01. Lab-Abstraction/03.StudentSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _03.StudentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentSystem studentSystem = new StudentSystem();            
+            studentSystem.ParseCommands();            
+        }
+    }
+}
---
+++ C#-OOP/01. Lab-Abstraction/03.StudentSystem/StudentSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.StudentSystem
+{
+    public class StudentSystem
+    {
+        private StudentsDatabase students;
+
+        public StudentSystem()
+        {
+            this.students = new StudentsDatabase();
+        }
+
+        public void ParseCommands()
+        {
+            while (true)
+            {
+                string[] args = Console.ReadLine().Split();
+                string commandName = args[0];
+
+                if (commandName == "Create")
+                {
+                    this.Create(args);
+                }
+                else if (commandName == "Show")
+                {
+                    this.Show(args);
+                }
+                else if (commandName == "Exit")
+                {
+                    return;
+                }
+            }
+        }
+
+        private void Show(string[] args)
+        {
+            string name = args[1];
+            Student student = this.students.Find(name);
+            if (student != null)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        private void Create(string[] args)
+        {
+            string name = args[1];
+            int age = int.Parse(args[2]);
+            double grade = double.Parse(args[3]);
+            this.students.Add(name, age, grade);
+        }
+    }
+}
