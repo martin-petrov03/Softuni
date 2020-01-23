b6b03a1fab5ff7e54e1586ab7e9ad696a7abb493
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-23 20:31:54 +0200

    Add 04.HotelReservation Problem

---
+++ C#-OOP/01. Lab-Abstraction/04.HotelReservation/PriceCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public enum SeasonMultiplier
+{
+    Autumn = 1,
+    Spring,
+    Winter,
+    Summer
+}
+
+public enum DiscountPercentage
+{
+    None,
+    SecondVisit = 10,
+    VIP = 20
+}
+
+public class PriceCalculator
+{
+    private decimal pricePerNight;
+    private int nights;
+    private SeasonMultiplier seasonMultiplier;
+    private DiscountPercentage discountPercentage;
+
+    public PriceCalculator(string[] commandArgs)
+    {
+        pricePerNight = decimal.Parse(commandArgs[0]);
+        nights = int.Parse(commandArgs[1]);
+        seasonMultiplier = Enum.Parse<SeasonMultiplier>(commandArgs[2]);
+        discountPercentage = DiscountPercentage.None;
+
+        if (commandArgs.Length == 4)
+        {
+            discountPercentage = Enum.Parse<DiscountPercentage>(commandArgs[3]);
+        }
+    }
+
+    public decimal GetTotalPrice()
+    {
+        decimal basePrice = pricePerNight * nights * (int)seasonMultiplier;
+
+        return basePrice - basePrice * (decimal)discountPercentage / 100;
+    }
+}
---
+++ C#-OOP/01. Lab-Abstraction/04.HotelReservation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.HotelReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+            .Split();
+            PriceCalculator priceCalculator = new PriceCalculator(input);            
+            Console.WriteLine($"{priceCalculator.GetTotalPrice():f2}");
+        }        
+    }
+}
