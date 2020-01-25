d366e9f8e47737256ead36d1bd80beaaee6cd81c
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-25 23:02:28 +0200

    Add P01_RawData Problem

---
+++ C#-OOP/02. Exercise-Abstraction/P01_RawData/Runner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_RawData
+{
+    public class Runner
+    {
+        public void Run()
+        {
+            List<Car> cars = new List<Car>();
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] parameters = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                GenerateCar(cars, parameters);
+            }
+
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                List<string> fragile = cars
+                    .Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(y => y.Pressure < 1))
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, fragile));
+            }
+            else
+            {
+                List<string> flamable = cars
+                    .Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250)
+                    .Select(x => x.Model)
+                    .ToList();
+
+                Console.WriteLine(string.Join(Environment.NewLine, flamable));
+            }
+        }
+
+        private void GenerateCar(List<Car> cars, string[] parameters)
+        {
+            Tire[] tires = new Tire[4];
+
+            string model = parameters[0];
+
+            int engineSpeed = int.Parse(parameters[1]);
+            int enginePower = int.Parse(parameters[2]);
+            Engine engine = new Engine(engineSpeed, enginePower);
+
+            int cargoWeight = int.Parse(parameters[3]);
+            string cargoType = parameters[4];
+            Cargo cargo = new Cargo(cargoWeight, cargoType);
+
+            double firstTirePressure = double.Parse(parameters[5]);
+            int firstTireAge = int.Parse(parameters[6]);
+            tires[0] = new Tire(firstTireAge, firstTirePressure);
+
+            double secondTirePressure = double.Parse(parameters[7]);
+            int secondTireAge = int.Parse(parameters[8]);
+            tires[1] = new Tire(secondTireAge, secondTirePressure);
+
+            double ThirdTirePressure = double.Parse(parameters[9]);
+            int thirdTireAge = int.Parse(parameters[10]);
+            tires[2] = new Tire(thirdTireAge, ThirdTirePressure);
+
+            double fourthTirePressure = double.Parse(parameters[11]);
+            int fourthTireAge = int.Parse(parameters[12]);
+            tires[3] = new Tire(fourthTireAge, fourthTirePressure);
+
+            cars.Add(new Car(model, engine, cargo, tires));
+        }
+    }
+}
---
+++ C#-OOP/02. Exercise-Abstraction/P01_RawData/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_RawData
+{    
+    public class RawData
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+}
