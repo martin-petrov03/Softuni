ece87d056467994ecd15d2715a2771f8b62476fa
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-25 23:02:54 +0200

    Add P02_CarsSalesman Problem

---
+++ C#-OOP/02. Exercise-Abstraction/P02_CarsSalesman/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P02_CarsSalesman
+{    
+    public class CarSalesman
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+}
