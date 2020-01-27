6e9860acd43dfde96f79c9fb7acc574867c27258
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-27 23:38:28 +0200

    Add P03_JediGalaxy Problem

---
+++ C#-OOP/02. Exercise-Abstraction/P03_JediGalaxy/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_JediGalaxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = dimestions[0];
+            int m = dimestions[1];
+
+            int[,] matrix = GenerateMatrix(n, m);
+
+            string command = Console.ReadLine();
+            long sum = 0;
+            while (command != "Let the Force be with you")
+            {
+                int[] ivoCoordinates = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int[] evil = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int evilRow = evil[0];
+                int evilCol = evil[1];
+
+                while (evilRow >= 0 && evilCol >= 0)
+                {
+                    if (evilRow >= 0 && evilRow < matrix.GetLength(0) && evilCol >= 0 && evilCol < matrix.GetLength(1))
+                    {
+                        matrix[evilRow, evilCol] = 0;
+                    }
+                    evilRow--;
+                    evilCol--;
+                }
+
+                int ivoRow = ivoCoordinates[0];
+                int ivoCol = ivoCoordinates[1];
+
+                while (ivoRow >= 0 && ivoCol < matrix.GetLength(1))
+                {
+                    if (ivoRow >= 0 && ivoRow < matrix.GetLength(0) && ivoCol >= 0 && ivoCol < matrix.GetLength(1))
+                    {
+                        sum += matrix[ivoRow, ivoCol];
+                    }
+
+                    ivoCol++;
+                    ivoRow--;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static int[,] GenerateMatrix(int n, int m)
+        {
+            int[,] matrix = new int[n, m];
+            int value = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = value++;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
