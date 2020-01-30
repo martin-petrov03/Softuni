44c0291dfcf880e37be9b6412e103252076930d9
martinpetrov000 <martin.petrov033@gmail.com>
2020-01-30 09:06:47 +0200

    Add 02.StackOfStrings Problem

---
+++ C#-OOP/03. Lab-Inheritance/02.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public void AddRange(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                this.Push(item);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+    }
+}
---
+++ C#-OOP/03. Lab-Inheritance/02.StackOfStrings/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomStack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new[] { "1", "2", "4" });
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
