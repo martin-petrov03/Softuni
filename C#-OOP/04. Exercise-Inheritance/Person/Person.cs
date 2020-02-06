79defbbaddf0d12a4b8cfaf694695c8a3b7ec30b
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-06 19:41:12 +0200

    Add Person Problem

---
+++ C#-OOP/04. Exercise-Inheritance/Person/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+
+            return sb.ToString();
+        }
+    }
+}
