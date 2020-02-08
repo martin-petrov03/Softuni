d763b5be66dbd6350e309c9e5ad8b8ca4e2a9c8b
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-08 20:18:32 +0200

    Restaurant

---
+++ C#-OOP/04. Exercise-Inheritance/Restaurant/Beverages/Beverage.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        protected Beverage(string name, decimal price, double milliliters) 
+            : base(name, price)
+        {
+            this.Milliliters = milliliters;
+        }
+
+        public double Milliliters { get; set; }
+    }
+}
---
+++ C#-OOP/04. Exercise-Inheritance/Restaurant/FoodProducts/Food.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Food : Product
+    {
+        protected Food(string name, decimal price, double grams) 
+            : base(name, price)
+        {
+            this.Grams = grams;
+        }
+
+        public virtual double Grams { get; set; }
+    }
+}
