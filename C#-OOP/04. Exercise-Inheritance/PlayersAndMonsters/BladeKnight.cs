fb75c1cb47356992ca1ca1430006f6d22e895b97
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-09 15:08:17 +0200

    Add PlayersAndMonsters Problem

---
+++ C#-OOP/04. Exercise-Inheritance/PlayersAndMonsters/BladeKnight.cs
@@ -0,0 +1,10 @@
+﻿namespace PlayersAndMonsters
+{
+    public class BladeKnight : Knight
+    {
+        public BladeKnight(string username, int level) 
+            : base(username, level)
+        {
+        }
+    }
+}
---
+++ C#-OOP/04. Exercise-Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Hero gosho = new BladeKnight("Gosho", 12);
+            Hero pesho = new DarkWizard("Pesho", 12);
+
+            Console.WriteLine(gosho);
+            Console.WriteLine(pesho);
+        }
+    }
+}
