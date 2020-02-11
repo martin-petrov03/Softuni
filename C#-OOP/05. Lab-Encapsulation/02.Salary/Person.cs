4e8628a6f5ad37eab80fcd0134d0b2d192a7d8f1
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-11 18:24:27 +0200

    Add 02.Salary Problem

---
+++ C#-OOP/05. Lab-Encapsulation/02.Salary/Person.cs
@@ -0,0 +1,35 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        public int Age { get { return this.age; } }
+
+        public override string ToString()
+        {
+            return $"{this.firstName} {this.lastName} receives {this.salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percent)
+        {
+            if (this.Age < 30)
+            {
+                percent = percent / 2;
+            }
+
+            this.salary *= 1 + (percent / 100.0M);
+        }
+    }
+}
