871ee13dfd694705b92c15affc6bcfd16967062a
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-13 20:50:08 +0200

    Add 04.FirstAndReserveTeam Problem

---
+++ C#-OOP/05. Lab-Encapsulation/04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstSquad;
+        private List<Person> reserveSquad;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstSquad = new List<Person>();
+            this.reserveSquad = new List<Person>();
+        }        
+
+        public IReadOnlyCollection<Person> FirstTeam =>
+            this.firstSquad.AsReadOnly();
+
+        public IReadOnlyCollection<Person> ReserveTeam =>
+            this.reserveSquad.AsReadOnly();        
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                this.firstSquad.Add(player);
+            }
+            else
+            {
+                this.reserveSquad.Add(player);
+            }
+        }
+    }
+}
