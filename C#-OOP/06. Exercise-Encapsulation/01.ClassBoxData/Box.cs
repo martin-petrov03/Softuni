0d3b582c329f83c3bebf9ae3754ecea09d4abd69
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-15 20:05:38 +0200

    Add 01.ClassBoxData Problem

---
+++ C#-OOP/06. Exercise-Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public void printSurfaceArea()
+        {
+            double area = 2 * this.length * this.width + 2 * this.length * this.height + 2 * this.width * this.height;
+            Console.WriteLine($"Surface Area - {area:F2}");
+        }
+
+        public void printLateralSurfaceArea()
+        {
+            double area = 2 * this.length * this.height + 2 * this.width * this.height;
+            Console.WriteLine($"Lateral Surface Area - {area:F2}");
+        }
+
+        public void printVolume()
+        {
+            double area = this.length * this.width * this.height;
+            Console.WriteLine($"Volume - {area:F2}");
+        }
+    }
+}
---
+++ C#-OOP/06. Exercise-Encapsulation/01.ClassBoxData/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            if (length <= 0 || width <= 0 || height <= 0)
+            {
+                Console.WriteLine("Width cannot be zero or negative.");
+                return;
+            }
+
+            Box box = new Box(length, width, height);
+            box.printSurfaceArea();
+            box.printLateralSurfaceArea();
+            box.printVolume();
+        }
+    }
+}
