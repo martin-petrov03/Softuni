7edfe66c24da70521d63fdca88af416170d1d3b1
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-18 22:52:52 +0200

    Add 01.Shapes Problem

---
+++ C#-OOP/07. Lab-Interfaces-Abstraction/01.Shapes/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public int Radius
+        {
+            get
+            {
+                return this.radius;
+            }
+
+            private set
+            {
+                this.radius = value;
+            }
+        }        
+
+        public void Draw()
+        {            
+            for (int row = 0; row < this.Radius * 2; row++)
+            {
+                for (int col = 0; col < this.Radius * 2; col++)
+                {
+                    double distance = Math.Ceiling(Math.Sqrt((row - this.Radius) * (row - this.Radius) + (col - this.Radius) * (col - this.Radius)));
+               
+                    if (distance == this.Radius)
+                    {
+                        Console.Write("**");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }            
+        }
+    }
+}
---
+++ C#-OOP/07. Lab-Interfaces-Abstraction/01.Shapes/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                this.width = value;
+            }
+        }
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                this.height = value;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine(new string('*', this.Width));
+            for (int i = 0; i < this.Height - 2; i++)
+            {
+                Console.WriteLine('*' + new string(' ', this.Width - 2) + '*');
+            }
+            Console.WriteLine(new string('*', this.Width));
+        }
+    }
+}
