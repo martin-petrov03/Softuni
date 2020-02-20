78227e5bc144a9410a52d9a5dca7314bd5b5adb6
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-20 16:36:58 +0200

    Add 02.ExplicitInterfaces Problem

---
+++ C#-OOP/08. Exercise-Interfaces-Abstraction/02.ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,28 @@
+﻿namespace ExplicitInterfaces
+{
+    public class Citizen : IPerson, IResident
+    {
+        public Citizen(string name, string country, int age)
+        {
+            Name = name;
+            Country = country;
+            Age = age;
+        }
+
+        public string Name { get; }
+
+        public int Age { get; }
+
+        public string Country { get; }
+
+        string IPerson.GetName()
+        {
+            return this.Name;
+        }
+
+        string IResident.GetName()
+        {
+            return $"Mr/Ms/Mrs {this.Name}";
+        }
+    }
+}
---
+++ C#-OOP/08. Exercise-Interfaces-Abstraction/02.ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] inputInfo = input.Split();
+                string name = inputInfo[0];
+                string country = inputInfo[1];
+                int age = int.Parse(inputInfo[2]);
+
+                Citizen citizen = new Citizen(name, country, age);
+                IPerson person = citizen;
+                IResident resident = citizen;
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+
+                input = Console.ReadLine();
+            }                        
+        }
+    }
+}
