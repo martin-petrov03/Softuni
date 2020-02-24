aa1e0c005996659227f4f093b7eda7d85a23e28d
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-24 20:05:34 +0200

    Add 06.BorderControl Problem

---
+++ C#-OOP/08. Exercise-Interfaces-Abstraction/06.BorderControl/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var detained = new HashSet<string>();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] items = input.Split();
+                string id;
+
+                if (items.Length == 2)
+                {                    
+                    id = items[1];
+                }
+                else
+                {                    
+                    id = items[2];
+                }
+
+                detained.Add(id);
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+
+            PrintDetainedIds(detained, input);
+        }
+
+        private static void PrintDetainedIds(HashSet<string> hashset, string searchedId)
+        {
+            foreach (string id in hashset)
+            {
+                if (id.EndsWith(searchedId))
+                {
+                    Console.WriteLine(id);
+                }
+            }
+        }
+    }
+}
