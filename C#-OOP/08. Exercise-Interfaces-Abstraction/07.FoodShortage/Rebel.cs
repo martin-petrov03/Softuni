dfcc06ee92c110433080a3e52e0f5c43a78ba327
martinpetrov000 <martin.petrov033@gmail.com>
2020-02-24 20:15:28 +0200

    Add 07.FoodShortage Problem

---
+++ C#-OOP/08. Exercise-Interfaces-Abstraction/07.FoodShortage/Rebel.cs
@@ -0,0 +1,20 @@
+﻿namespace FoodShortage
+{
+    public class Rebel : IBuyer
+    {
+        public Rebel(string name, int age, string group)
+        {
+            this.Name = name;
+            this.Food = 0;
+        }
+
+        public string Name { get; }
+
+        public int Food { get; private set; }
+
+        public void BuyFood()
+        {
+            this.Food += 5;
+        }
+    }
+}
