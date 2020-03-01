eeea7ec4b81c988dee27d91b0a6355842d217ffa
martinpetrov000 <martin.petrov033@gmail.com>
2020-03-01 18:47:33 +0200

    Add 03.Shapes Problem

---
+++ C#-OOP/09. Lab-Polymorphism/03.Shapes/Rectangle.cs
@@ -0,0 +1,41 @@
+﻿namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set { width = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set { height = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return this.height * this.width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.height * 2 +  this.width * 2;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }
+    }
+}
---
+++ C#-OOP/09. Lab-Polymorphism/03.Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(12);
+            Shape rectangle = new Rectangle(12, 12);
+
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine(rectangle.Draw());            
+        }
+    }
+}
