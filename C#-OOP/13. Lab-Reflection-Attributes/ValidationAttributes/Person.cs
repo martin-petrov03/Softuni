ddec6c87b4dc28b624825d5bdde28f218770a112
martinpetrov000 <martin.petrov033@gmail.com>
2020-03-08 20:00:54 +0200

    Add ValidationAttributes Problem

---
+++ C#-OOP/13. Lab-Reflection-Attributes/ValidationAttributes/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+
+    public class Person
+    {
+        public Person(string fullName, int age)
+        {
+            this.FullName = fullName;
+            this.Age = age;
+        }
+
+        [MyRequired]
+        [Obsolete]
+        public string FullName { get; set; }
+
+        [MyRange(18, 65)]
+        public int Age { get; set; }
+    }
+}
