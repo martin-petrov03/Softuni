fec728caa97996bc700aae746a6e782d33819fdc
martinpetrov000 <martin.petrov033@gmail.com>
2020-03-09 10:24:15 +0200

    Add

---
+++ C#-OOP/13. Lab-Reflection-Attributes/CommandPattern/Core/Commands/OpenCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace CommandPattern.Core.Commands
+{    
+    using Contracts;
+
+    public class OpenCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string path = args[0];
+
+            System.Diagnostics.Process.Start("cmd", $"/c start {path}");
+            return "Started successfully!";
+        }
+    }
+}
