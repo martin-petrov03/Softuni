7de5cfcdadbac0db57b97557a932fe119d5882e5
martinpetrov000 <martin.petrov033@gmail.com>
2020-05-09 14:16:53 +0300

    Add 02.VillainNames

---
+++ Entity-Framework-Core/01. DB-Apps-Introduction/02.VillainNames/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace _02.VillainNames
+{
+    class StartUp
+    {
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=True";
+
+        public static void Main()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                string selectCommand =
+                    "SELECT v.Name, COUNT(*) AS Count FROM MinionsVillains mv " +
+                    "JOIN Villains v ON v.Id = mv.VillainId GROUP BY v.Id, v.Name " +
+                    "HAVING COUNT(*) > 3 ORDER BY Count DESC";
+
+                using (SqlCommand command = new SqlCommand(selectCommand, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader[0]} - {reader[1]}");
+                        }
+                    }
+
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
