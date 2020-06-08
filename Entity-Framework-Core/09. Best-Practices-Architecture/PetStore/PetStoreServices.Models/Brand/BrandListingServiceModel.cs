743ae1822949705198c5e964f8232a1a10b1e3b2
martinpetrov000 <martin.petrov033@gmail.com>
2020-06-08 12:14:45 +0300

    Add ServiceModels

---
+++ Entity-Framework-Core/09. Best-Practices-Architecture/PetStore/PetStoreServices.Models/Brand/BrandListingServiceModel.cs
@@ -0,0 +1,19 @@
+﻿namespace PetStoreServices.Models.Brand
+{
+    public class BrandListingServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public double Profit { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int BrandId { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ Entity-Framework-Core/09. Best-Practices-Architecture/PetStore/PetStoreServices.Models/Pet/PetListingServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PetStoreServices.Models.Pet
+{
+    public class PetListingServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Category { get; set; }
+
+        public string Breed { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
